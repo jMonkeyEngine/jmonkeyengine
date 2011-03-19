1871d02b7d63360b983cc0e4d7d08a4213f5d67f
PSp..om <PSpeed42@gmail.com>
2011-03-19 15:44:42 +0000

    Fixed a port typo after all the cutting pasting and fixing last night.  UDP connection now setup with the proper remote UDP port.
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7042 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/networking/com/jme3/network/Network.java
+++ engine/src/networking/com/jme3/network/Network.java
@@ -164,7 +164,7 @@ public class Network
                                           int localUdpPort ) throws IOException
     {
         InetAddress remoteAddress = InetAddress.getByName(host);   
-        UdpConnector fast = new UdpConnector( localUdpPort, remoteAddress, hostPort ); 
+        UdpConnector fast = new UdpConnector( localUdpPort, remoteAddress, remoteUdpPort ); 
         SocketConnector reliable = new SocketConnector( remoteAddress, hostPort );        
        
         return new DefaultClient( gameName, version, reliable, fast );
@@ -187,7 +187,7 @@ public class Network
         public void connectToServer( InetAddress address, int port, int remoteUdpPort, 
                                      int localUdpPort ) throws IOException
         {
-            UdpConnector fast = new UdpConnector( localUdpPort, address, port ); 
+            UdpConnector fast = new UdpConnector( localUdpPort, address, remoteUdpPort ); 
             SocketConnector reliable = new SocketConnector( address, port );        
             
             setConnectors( reliable, fast );
