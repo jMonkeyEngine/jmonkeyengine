cdd44058d829b755bec672ab7cd9cfcfd3252c00
nor..67 <normen667@gmail.com>
2011-04-02 20:50:00 +0000

    - remove isActive from AppState interface
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7161 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/app/state/AppState.java
+++ engine/src/core/com/jme3/app/state/AppState.java
@@ -64,26 +64,6 @@ public interface AppState {
     public boolean isInitialized();
 
     /**
-     * Use setEnabled() instead.
-     * Activate or deactivate the functionality of the <code>AppState</code>.
-     * The effect of this call depends on implementation. An 
-     * <code>AppState</code> starts as being active by default.
-     * 
-     * @param active activate the AppState or not.
-     */
-    @Deprecated
-    public void setActive(boolean active);
-
-    /**
-     * Use isEnabled() instead.
-     * @return True if the <code>AppState</code> is active, false otherwise.
-     * 
-     * @see AppState#setActive(boolean)
-     */
-    @Deprecated
-    public boolean isActive();
-
-    /**
      * Enable or disable the functionality of the <code>AppState</code>.
      * The effect of this call depends on implementation. An 
      * <code>AppState</code> starts as being enabled by default.
