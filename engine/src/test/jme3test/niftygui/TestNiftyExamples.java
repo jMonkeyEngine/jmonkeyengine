e990b2582281a6592ff36fada4dd7173f91d2d8d
sha..rd <shadowislord@gmail.com>
2011-04-20 16:52:38 +0000

    * TestNiftyGui now uses jme3test/niftygui/hellojme.xml and acts as a ScreenController  * Added TestNiftyExamples that runs the all/intro.xml in the nifty examples jar
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7274 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/test/jme3test/niftygui/TestNiftyExamples.java
+++ engine/src/test/jme3test/niftygui/TestNiftyExamples.java
@@ -33,65 +33,33 @@
 package jme3test.niftygui;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.Vector3f;
 import com.jme3.niftygui.NiftyJmeDisplay;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 import de.lessvoid.nifty.Nifty;
-import de.lessvoid.nifty.screen.Screen;
-import de.lessvoid.nifty.screen.ScreenController;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class TestNiftyGui extends SimpleApplication implements ScreenController {
+public class TestNiftyExamples extends SimpleApplication {
 
     private Nifty nifty;
 
     public static void main(String[] args){
-        TestNiftyGui app = new TestNiftyGui();
+        TestNiftyExamples app = new TestNiftyExamples();
         app.setPauseOnLostFocus(false);
         app.start();
     }
 
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setTexture("ColorMap", assetManager.loadTexture("Interface/Logo/Monkey.jpg"));
-        geom.setMaterial(mat);
-        rootNode.attachChild(geom);
-
         NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
                                                           inputManager,
                                                           audioRenderer,
                                                           guiViewPort);
         nifty = niftyDisplay.getNifty();
 
-        nifty.fromXml("jme3test/niftygui/hellojme.xml", "start", this);
+        nifty.fromXml("all/intro.xml", "start");
 
         // attach the nifty display to the gui view port as a processor
         guiViewPort.addProcessor(niftyDisplay);
 
         // disable the fly cam
-//        flyCam.setEnabled(false);
-        flyCam.setDragToRotate(true);
-    }
-
-    public void bind(Nifty nifty, Screen screen) {
-        System.out.println("bind( " + screen.getScreenId() + ")");
-    }
-
-    public void onStartScreen() {
-        System.out.println("onStartScreen");
-    }
-
-    public void onEndScreen() {
-        System.out.println("onEndScreen");
-    }
-
-    public void quit(){
-        nifty.gotoScreen("end");
+        flyCam.setEnabled(false);
     }
 
 }
