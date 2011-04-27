ba6419f924429945e6f5a5b8d423bb4b0434fa9e
nor..67 <normen667@gmail.com>
2011-04-27 19:59:36 +0000

    - fix TestBoneRagdoll init order for native physics
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7341 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/test/jme3test/bullet/TestBoneRagdoll.java
+++ engine/src/test/jme3test/bullet/TestBoneRagdoll.java
@@ -94,10 +94,6 @@ public class TestBoneRagdoll extends SimpleApplication implements RagdollCollisi
         initCrossHairs();
         initMaterial();
 
-        bullet = new Sphere(32, 32, 1.0f, true, false);
-        bullet.setTextureMode(TextureMode.Projected);
-        bulletCollisionShape = new SphereCollisionShape(1.0f);
-
         cam.setLocation(new Vector3f(0.26924422f, 6.646658f, 22.265987f));
         cam.setRotation(new Quaternion(-2.302544E-4f, 0.99302495f, -0.117888905f, -0.0019395084f));
 
@@ -105,6 +101,10 @@ public class TestBoneRagdoll extends SimpleApplication implements RagdollCollisi
         bulletAppState = new BulletAppState();
         bulletAppState.setEnabled(true);
         stateManager.attach(bulletAppState);
+        bullet = new Sphere(32, 32, 1.0f, true, false);
+        bullet.setTextureMode(TextureMode.Projected);
+        bulletCollisionShape = new SphereCollisionShape(1.0f);
+
         bulletAppState.getPhysicsSpace().enableDebug(assetManager);
         PhysicsTestHelper.createPhysicsTestWorld(rootNode, assetManager, bulletAppState.getPhysicsSpace());
         setupLight();
