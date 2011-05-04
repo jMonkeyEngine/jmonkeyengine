8b2b999bd0cff2ee659b4c2d51a8b187685b3508
Sha..om <ShadowIsLord@gmail.com>
2011-05-04 16:19:51 +0000

    * Added proper check for framebuffer textures when doing NPOT scaling.
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7441 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/lwjgl-ogl/com/jme3/renderer/lwjgl/LwjglRenderer.java
+++ engine/src/lwjgl-ogl/com/jme3/renderer/lwjgl/LwjglRenderer.java
@@ -1715,8 +1715,11 @@ public class LwjglRenderer implements Renderer {
         }
 
         // Yes, some OpenGL2 cards (GeForce 5) still dont support NPOT.
-        if (!GLContext.getCapabilities().GL_ARB_texture_non_power_of_two){
-            if (img.getWidth() != 0 && img.getHeight() != 0){
+          if (!GLContext.getCapabilities().GL_ARB_texture_non_power_of_two){
+            if (img.getData(0) == null)
+                throw new RendererException("non-power-of-2 framebuffer textures are not supported by the video hardware");
+            
+            if (img.getWidth() != 0 && img.getHeight() != 0){ 
                 if (!FastMath.isPowerOfTwo(img.getWidth())
                     || !FastMath.isPowerOfTwo(img.getHeight())
                     || img.getWidth() != img.getHeight()){
