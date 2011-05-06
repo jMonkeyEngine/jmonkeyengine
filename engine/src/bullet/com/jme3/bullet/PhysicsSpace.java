217eacde1ecd9e0230890cd729bd44b7ca774ea0
nor..67 <normen667@gmail.com>
2011-05-06 18:02:19 +0000

    - fix PhysicsSpace.removeAll() recursion bug (same fix as for addAll) Fixes Issue 349
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7456 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/bullet/com/jme3/bullet/PhysicsSpace.java
+++ engine/src/bullet/com/jme3/bullet/PhysicsSpace.java
@@ -537,9 +537,7 @@ public class PhysicsSpace {
             List<Spatial> children = ((Node) spatial).getChildren();
             for (Iterator<Spatial> it = children.iterator(); it.hasNext();) {
                 Spatial spat = it.next();
-                if (spat instanceof Node) {
-                    removeAll((Node) spat);
-                }
+                removeAll((Node) spat);
             }
         }
     }
