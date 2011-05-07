bef4a771690be9d0b4861958f49e7b84ec4d9a54
sha..rd <shadowislord@gmail.com>
2011-05-07 05:48:44 +0000

    * Fixed viewport location bug in RenderManager
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7461 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/renderer/RenderManager.java
+++ engine/src/core/com/jme3/renderer/RenderManager.java
@@ -611,15 +611,19 @@ public class RenderManager {
             cam.clearViewportChanged();
             prevCam = cam;
 
-
-//            float translateX = viewWidth == viewX ? 0 : -(viewWidth + viewX) / (viewWidth - viewX);
-//            float translateY = viewHeight == viewY ? 0 : -(viewHeight + viewY) / (viewHeight - viewY);
-//            float scaleX = viewWidth == viewX ? 1f : 2f / (viewWidth - viewX);
-//            float scaleY = viewHeight == viewY ? 1f : 2f / (viewHeight - viewY);
+            float translateX = viewWidth == viewX ? 0 : -(viewWidth + viewX) / (viewWidth - viewX);
+            float translateY = viewHeight == viewY ? 0 : -(viewHeight + viewY) / (viewHeight - viewY);
+            float scaleX = viewWidth == viewX ? 1f : 2f / (viewWidth - viewX);
+            float scaleY = viewHeight == viewY ? 1f : 2f / (viewHeight - viewY);
+            
             orthoMatrix.loadIdentity();
-            orthoMatrix.setTranslation(-1f, -1f, 0f);
-            orthoMatrix.setScale(2f / cam.getWidth(), 2f / cam.getHeight(), 0f);
-            System.out.println(orthoMatrix);
+            orthoMatrix.setTranslation(translateX, translateY, 0);
+            orthoMatrix.setScale(scaleX, scaleY, 1); 
+            
+            //orthoMatrix.loadIdentity();
+            //orthoMatrix.setTranslation(-1f, -1f, 0f);
+            //orthoMatrix.setScale(2f / cam.getWidth(), 2f / cam.getHeight(), 0f);
+            //System.out.println(orthoMatrix);
         }
     }
 
