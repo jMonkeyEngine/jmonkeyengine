d4baac1e6b20af1c37df1c92c79f119a473e383c
nor..67 <normen667@gmail.com>
2011-06-12 11:25:34 +0000

    - fix material error
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7596 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- sdk/jme3-core/src/com/jme3/gde/core/scene/processors/WireProcessor.java
+++ sdk/jme3-core/src/com/jme3/gde/core/scene/processors/WireProcessor.java
@@ -50,7 +50,8 @@ public class WireProcessor implements SceneProcessor {
     Material wireMaterial;
 
     public WireProcessor(AssetManager manager) {
-        wireMaterial = new Material(manager, "/Common/MatDefs/Misc/WireColor.j3md");
+        wireMaterial = new Material(manager, "/Common/MatDefs/Misc/Unshaded.j3md");
+        wireMaterial.getAdditionalRenderState().setWireframe(true);
         wireMaterial.setColor("Color", ColorRGBA.Blue);
     }
 
