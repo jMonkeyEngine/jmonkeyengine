4dffbc95f52799e4bd22bb871cb00e59af7fc329
sha..rd <shadowislord@gmail.com>
2011-06-25 17:59:34 +0000

    * Forgot to commit this file
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7728 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ engine/src/core/com/jme3/export/FormatVersion.java
@@ -0,0 +1,10 @@
+package com.jme3.export;
+
+public final class FormatVersion {
+    
+    public static final int VERSION = 1;
+    public static final int SIGNATURE = 0x4A4D4533;
+    
+    private FormatVersion(){
+    }
+}
