324750e4aa2a2750a0d4e37acfa63ca5a3e29d9c
nor..67 <normen667@gmail.com>
2011-07-02 12:32:09 +0000

    - fix polyoffset in "create j3m" generated files
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7807 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- sdk/jme3-materialeditor/src/com/jme3/gde/materials/EditableMaterialFile.java
+++ sdk/jme3-materialeditor/src/com/jme3/gde/materials/EditableMaterialFile.java
@@ -480,7 +480,9 @@ public class EditableMaterialFile {
         additionalRenderStates.put("FaceCull", new MaterialProperty("FaceCullMode", "FaceCull", mat.getAdditionalRenderState().getFaceCullMode().name()));
         additionalRenderStates.put("Blend", new MaterialProperty("BlendMode", "Blend", mat.getAdditionalRenderState().getBlendMode().name()));
         additionalRenderStates.put("AlphaTestFalloff", new MaterialProperty("Float", "AlphaTestFalloff", mat.getAdditionalRenderState().getAlphaFallOff() + ""));
-        additionalRenderStates.put("PolyOffset", new MaterialProperty("Float,Float", "PolyOffset", mat.getAdditionalRenderState().getPolyOffsetUnits() + " " + mat.getAdditionalRenderState().getPolyOffsetFactor()));
+        //TODO: workaround
+        additionalRenderStates.put("PolyOffset", new MaterialProperty("Float,Float", "PolyOffset", "0.0 0.0"));
+//        additionalRenderStates.put("PolyOffset", new MaterialProperty("Float,Float", "PolyOffset", mat.getAdditionalRenderState().getPolyOffsetUnits() + " " + mat.getAdditionalRenderState().getPolyOffsetFactor()));
         checkWithMatDef();
         setAsText(getUpdatedContent());
     }
