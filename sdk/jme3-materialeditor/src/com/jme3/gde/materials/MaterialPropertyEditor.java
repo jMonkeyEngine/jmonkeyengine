9b466549632beb03daff6a18086b9ad4f9e16d80
rem..om <remy.bouquet@gmail.com>
2011-07-28 12:29:45 +0000

    SDK : - fixed materialPropertyEditor that was failing when changing material and aplying it to the geom
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@7933 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- sdk/jme3-materialeditor/src/com/jme3/gde/materials/MaterialPropertyEditor.java
+++ sdk/jme3-materialeditor/src/com/jme3/gde/materials/MaterialPropertyEditor.java
@@ -138,7 +138,7 @@ public class MaterialPropertyEditor implements PropertyEditor, SceneExplorerProp
                 public Void call() throws Exception {
                     SceneRequest request = SceneApplication.getApplication().getCurrentSceneRequest();
                     ((DesktopAssetManager) request.getManager()).deleteFromCache(new AssetKey(text));
-                    Material localMaterial = (Material) request.getManager().loadAsset(text);
+                    Material localMaterial = (Material) request.getManager().loadMaterial(text);
                     if (localMaterial != null) {
                         material = localMaterial;
                     }
