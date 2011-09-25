780da5e5812178797eb070c81d41b2ebbde1ddcc
Kae..pl <Kaelthas_Spellsinger@o2.pl>
2011-09-25 22:22:07 +0000

    Tests for loading bone and object animation from blender added.
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8306 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ engine/src/test/jme3test/model/anim/TestBlenderObjectAnim.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2009-2010 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package jme3test.model.anim;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.BlenderKey;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
+public class TestBlenderObjectAnim extends SimpleApplication {
+
+    private AnimChannel channel;
+    private AnimControl control;
+
+    public static void main(String[] args) {
+    	TestBlenderObjectAnim app = new TestBlenderObjectAnim();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        flyCam.setMoveSpeed(10f);
+        cam.setLocation(new Vector3f(6.4013605f, 7.488437f, 12.843031f));
+        cam.setRotation(new Quaternion(-0.060740203f, 0.93925786f, -0.2398315f, -0.2378785f));
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setDirection(new Vector3f(-0.1f, -0.7f, -1).normalizeLocal());
+        dl.setColor(new ColorRGBA(1f, 1f, 1f, 1.0f));
+        rootNode.addLight(dl);
+
+        BlenderKey blenderKey = new BlenderKey("Blender/2.4x/animtest.blend");
+        
+        Spatial scene = (Spatial) assetManager.loadModel(blenderKey);
+        rootNode.attachChild(scene);
+        
+        Spatial model = this.findNode(rootNode, "TestAnim");
+        model.center();
+        
+        control = model.getControl(AnimControl.class);
+        channel = control.createChannel();
+
+        channel.setAnim("TestAnim");
+    }
+    
+    /**
+     * This method finds a node of a given name.
+     * @param rootNode the root node to search
+     * @param name the name of the searched node
+     * @return the found node or null
+     */
+    private Spatial findNode(Node rootNode, String name) {
+        if (name.equals(rootNode.getName())) {
+            return rootNode;
+        }
+        return rootNode.getChild(name);
+    }
+}
