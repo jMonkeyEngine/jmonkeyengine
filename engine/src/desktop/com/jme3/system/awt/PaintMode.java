6df9eaf033559ef30b4a22c35661eb54c40262d9
nor..67 <normen667@gmail.com>
2011-10-04 12:35:48 +0000

    - add backengineered PaintMode.java ;)
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8345 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ engine/src/desktop/com/jme3/system/awt/PaintMode.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jme3.system.awt;
+
+/**
+ *
+ * @author normenhansen
+ */
+public enum PaintMode {
+    Accelerated,
+    OnRequest,
+    Repaint;
+    
+}
