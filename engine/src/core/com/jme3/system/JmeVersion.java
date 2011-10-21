3ff2771a69631c953838e36ee2e887162a9598c7
nor..67 <normen667@gmail.com>
2011-10-21 12:01:02 +0000

    - add backengineered JmeVersion to fix build
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8476 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ engine/src/core/com/jme3/system/JmeVersion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jme3.system;
+
+/**
+ *
+ * @author normenhansen
+ */
+public class JmeVersion {
+    public static final String FULL_NAME = "jMonkeyEngine 3.0beta";
+    
+}
