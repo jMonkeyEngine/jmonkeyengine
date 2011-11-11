3db3648866585145c777ee51a256c1972e07eead
sha..rd <shadowislord@gmail.com>
2011-11-11 01:17:07 +0000

    * Fix spelling error in AmbientLight: uneffected -> unaffected
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8642 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/light/AmbientLight.java
+++ engine/src/core/com/jme3/light/AmbientLight.java
@@ -5,7 +5,7 @@ import com.jme3.scene.Spatial;
 /**
  * An ambient light adds a constant color to the scene.
  * <p>
- * Ambient lights are uneffected by the surface normal, and are constant
+ * Ambient lights are unaffected by the surface normal, and are constant
  * regardless of the model's location. The material's ambient color is
  * multiplied by the ambient light color to get the final ambient color of
  * an object.
