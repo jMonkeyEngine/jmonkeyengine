94ec216aab99ccc29920bfe61e83d34fbed9a954
nor..67 <normen667@gmail.com>
2011-11-15 20:27:51 +0000

    - fix TerrainQuadGrid support in TerrainGrid
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8681 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/terrain/com/jme3/terrain/geomipmap/TerrainGrid.java
+++ engine/src/terrain/com/jme3/terrain/geomipmap/TerrainGrid.java
@@ -147,7 +147,7 @@ public class TerrainGrid extends TerrainQuad {
                             q.setMaterial(material.clone());
                             log.log(Level.FINE, "Loaded TerrainQuad {0} from HeightMapGrid", q.getName());
                         } else if (terrainQuadGrid != null) {
-                            q = terrainQuadGrid.getTerrainQuadAt(location);
+                            q = terrainQuadGrid.getTerrainQuadAt(temp);
                             q.setMaterial(material);
                             log.log(Level.FINE, "Loaded TerrainQuad {0} from TerrainQuadGrid", q.getName());
                         }
