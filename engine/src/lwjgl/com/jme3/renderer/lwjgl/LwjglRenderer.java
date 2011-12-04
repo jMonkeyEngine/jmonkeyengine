28a10e239cd717754fd18171bbe291c15c2b5c2f
Sha..om <ShadowIsLord@gmail.com>
2011-12-04 21:34:44 +0000

    * Moved framebuffer exception INSIDE NPOT check instead of outside where it made no sense
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@8858 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/lwjgl/com/jme3/renderer/lwjgl/LwjglRenderer.java
+++ engine/src/lwjgl/com/jme3/renderer/lwjgl/LwjglRenderer.java
@@ -1837,17 +1837,14 @@ public class LwjglRenderer implements Renderer {
 
         // Yes, some OpenGL2 cards (GeForce 5) still dont support NPOT.
         if (!GLContext.getCapabilities().GL_ARB_texture_non_power_of_two) {
-            if (img.getData(0) == null) {
-                throw new RendererException("non-power-of-2 framebuffer textures are not supported by the video hardware");
-            }
-
             if (img.getWidth() != 0 && img.getHeight() != 0) {
                 if (!FastMath.isPowerOfTwo(img.getWidth())
                         || !FastMath.isPowerOfTwo(img.getHeight())) {
-//                    logger.log(Level.WARNING, "Encountered NPOT texture {0}, "
-//                                            + "it might not display correctly.", img);
-
-                    MipMapGenerator.resizeToPowerOf2(img);
+                    if (img.getData(0) == null) {
+                        throw new RendererException("non-power-of-2 framebuffer textures are not supported by the video hardware");
+                    } else {
+                        MipMapGenerator.resizeToPowerOf2(img);
+                    }
                 }
             }
         }
