b0cff4343e24eecef69b7fbdbe945dc77bba834a
Sha..om <ShadowIsLord@gmail.com>
2012-02-07 02:40:04 +0000

    * Fix View -> Model space for ViewProjectionMatrix
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@9137 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/shader/UniformBinding.java
+++ engine/src/core/com/jme3/shader/UniformBinding.java
@@ -73,7 +73,7 @@ public enum UniformBinding {
     WorldViewProjectionMatrix,
 
     /**
-     * The view projection matrix. Converts View space to Clip/Projection
+     * The view projection matrix. Converts Model space to Clip/Projection
      * space.
      * Type: mat4
      */
