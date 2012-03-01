50f266f8438dffb911f6271fc62a2bfeb884f54a
nor..67 <normen667@gmail.com>
2012-03-01 00:48:42 +0000

    - optimize BatchNode.batch a bit
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@9224 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/scene/BatchNode.java
+++ engine/src/core/com/jme3/scene/BatchNode.java
@@ -46,6 +46,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -190,10 +191,10 @@ public class BatchNode extends Node implements Savable {
             }
             batches.clear();
         }
-        
-        for (Material material : matMap.keySet()) {
+        for (Map.Entry<Material, List<Geometry>> entry : matMap.entrySet()) {
             Mesh m = new Mesh();
-            List<Geometry> list = matMap.get(material);
+            Material material = entry.getKey();
+            List<Geometry> list = entry.getValue();
             nbGeoms += list.size();
             if (!needsFullRebatch) {
                 list.add(batches.get(material).geometry);
