ad01d369153d690fe96e9df071119cbe5b89ff63
Sha..rd <ShadowIsLord@gmail.com>
2012-04-14 20:22:13 +0000

    * Added shader validation system. Requires either NVIDIA Cg or GPU Shader Analyser to be in the path. Run the ShaderCheck class to see which jME3 shaders fail validation.
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@9316 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ engine/src/tools/jme3tools/shadercheck/CgcValidator.java
@@ -0,0 +1,112 @@
+package jme3tools.shadercheck;
+
+import com.jme3.shader.Shader;
+import com.jme3.shader.Shader.ShaderSource;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CgcValidator implements Validator {
+
+    private static final Logger logger = Logger.getLogger(CgcValidator.class.getName());
+    private static String version;
+    
+    private static String checkCgCompilerVersion(){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cgc", "--version");
+            Process p = pb.start();
+            
+            Scanner scan = new Scanner(p.getErrorStream());
+            String ln = scan.nextLine();
+            scan.close();
+            
+            p.waitFor();
+            
+            String versionNumber = ln.split("\\s")[2];
+            return versionNumber.substring(0, versionNumber.length()-1);
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "IOEx", ex);
+        } catch (InterruptedException ex){
+        }
+        return null;
+    }
+    
+    public String getName() {
+        return "NVIDIA Cg Toolkit";
+    }
+
+    public boolean isInstalled() {
+        return getInstalledVersion() != null;
+    }
+
+    public String getInstalledVersion() {
+        if (version == null){
+            version = checkCgCompilerVersion();
+        }
+        return version;
+    }
+
+    private static void executeCg(String sourceCode, String language, String defines, String profile, StringBuilder output){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cgc", "-oglsl", 
+                                                          "-nocode", 
+                                                          "-strict", 
+                                                          "-glslWerror", 
+                                                          "-profile", profile, 
+                                                          //"-po", "NumMathInstructionSlots=64",// math instruction slots
+                                                          //"-po", "MaxTexIndirections=4",     // texture indirections
+                                                          "-po", "NumTemps=32",              // temporary variables
+                                                          //"-po", "NumInstructionSlots=1",    // total instruction slots
+                                                          //"-po", "NumTexInstructionSlots=32",// texture instruction slots 
+                                                          "-po", "MaxLocalParams=32");         // local parameters
+                                                          
+            
+            Process p = pb.start();
+            
+            String glslVer = language.substring(4);
+            
+            OutputStreamWriter writer = new OutputStreamWriter(p.getOutputStream());
+            writer.append("#version ").append(glslVer).append('\n');
+            writer.append("#extension all : warn").append('\n');
+            writer.append(defines).append('\n');
+            writer.write(sourceCode);
+            writer.close();
+            
+            Scanner scan = new Scanner(p.getErrorStream());
+            String ln = scan.nextLine();
+            if (ln.contains("0 errors")){
+                output.append(" - Success!").append('\n');
+            }else{
+                output.append(" - Failure!").append('\n');
+                output.append(ln).append('\n');
+                while (scan.hasNextLine()){
+                    output.append(scan.nextLine()).append('\n');
+                }
+            }
+            scan.close();
+
+            p.waitFor();
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "IOEx", ex);
+        } catch (InterruptedException ex){
+        }
+    }
+    
+    public void validate(Shader shader, StringBuilder results) {
+        String language = shader.getLanguage();
+        for (ShaderSource source : shader.getSources()){
+            results.append("Checking: ").append(source.getName());
+            switch (source.getType()){
+                case Fragment:
+                    executeCg(source.getSource(), language, source.getDefines(), "arbfp1", results);
+                    break;
+                case Vertex:
+                    executeCg(source.getSource(), language, source.getDefines(), "arbvp1", results);
+                    break;
+            }
+        }
+    }
+    
+}
---
+++ engine/src/tools/jme3tools/shadercheck/GpuAnalyzerValidator.java
@@ -0,0 +1,122 @@
+package jme3tools.shadercheck;
+
+import com.jme3.shader.Shader;
+import com.jme3.shader.Shader.ShaderSource;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Shader validator implementation for AMD's GPUShaderAnalyser.
+ * 
+ * @author Kirill Vainer
+ */
+public class GpuAnalyzerValidator implements Validator {
+
+    private static final Logger logger = Logger.getLogger(CgcValidator.class.getName());
+    private static String version;
+    
+    private static String checkGpuAnalyzerVersion(){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("GPUShaderAnalyzer", "-ListModules");
+            Process p = pb.start();
+            
+            Scanner scan = new Scanner(p.getInputStream());
+            String ln = scan.nextLine();
+            scan.close();
+            
+            p.destroy();
+            
+            return ln;
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "IOEx", ex);
+        }
+        return null;
+    }
+    
+    public String getName() {
+        return "AMD GPU Shader Analyzer";
+    }
+
+    public boolean isInstalled() {
+        return getInstalledVersion() != null;
+    }
+
+    public String getInstalledVersion() {
+        if (version == null){
+            version = checkGpuAnalyzerVersion();
+        }
+        return version;
+    }
+    private static void executeAnalyzer(String sourceCode, String language, String defines, String asic, StringBuilder results){
+        try {
+            // Export sourcecode to temporary file
+            File tempFile = File.createTempFile("test_shader", ".glsl");
+            FileWriter writer = new FileWriter(tempFile);
+            
+            String glslVer = language.substring(4);
+            writer.append("#version ").append(glslVer).append('\n');
+            writer.append("#extension all : warn").append('\n');
+            writer.append(defines).append('\n');
+            writer.write(sourceCode);
+            writer.close();
+
+            ProcessBuilder pb = new ProcessBuilder("GPUShaderAnalyzer", 
+                                                   tempFile.getAbsolutePath(),
+                                                   "-I",
+                                                   "-ASIC", asic);
+                                                   
+            Process p = pb.start();
+            
+            Scanner scan = new Scanner(p.getInputStream());
+            
+            if (!scan.hasNextLine()){
+                String x = scan.next();
+                System.out.println(x);
+            }
+            
+            String ln = scan.nextLine();
+            
+            if (ln.startsWith(";")){
+                results.append(" - Success!").append('\n');
+            }else{
+                results.append(" - Failure!").append('\n');
+                results.append(ln).append('\n');
+                while (scan.hasNextLine()){
+                    results.append(scan.nextLine()).append('\n');
+                }
+            }
+
+            scan.close();
+            p.getOutputStream().close();
+            p.getErrorStream().close();
+            
+            p.waitFor();
+            p.destroy();
+            
+            tempFile.delete();
+        } catch (InterruptedException ex) {
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "IOEx", ex);
+        }
+    }
+    
+    public void validate(Shader shader, StringBuilder results) {
+        String language = shader.getLanguage();
+        for (ShaderSource source : shader.getSources()){
+            results.append("Checking: ").append(source.getName());
+            switch (source.getType()){
+                case Fragment:
+                    executeAnalyzer(source.getSource(), language, source.getDefines(), "HD5770", results);
+                    break;
+                case Vertex:
+                    executeAnalyzer(source.getSource(), language, source.getDefines(), "HD5770", results);
+                    break;
+            }
+        }
+    }
+    
+}
