f8771f437e5942aa58231f37a1b30dfe5955b7c5
Kae..pl <Kaelthas_Spellsinger@o2.pl>
2012-05-13 14:49:50 +0000

    Fix to loading object animation for Blender 2.62+ (found by 'reactlogic').
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@9382 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/blender/com/jme3/scene/plugins/blender/animations/IpoHelper.java
+++ engine/src/blender/com/jme3/scene/plugins/blender/animations/IpoHelper.java
@@ -130,7 +130,7 @@ public class IpoHelper extends AbstractBlenderHelper {
 		if (rnaPath.endsWith("scale")) {
 			return Ipo.AC_SIZE_X + arrayIndex;
 		}
-		if (rnaPath.endsWith("rotation")) {
+		if (rnaPath.endsWith("rotation") || rnaPath.endsWith("rotation_euler")) {
 			return Ipo.OB_ROT_X + arrayIndex;
 		}
 		throw new IllegalStateException("Unknown curve rna path: " + rnaPath);
