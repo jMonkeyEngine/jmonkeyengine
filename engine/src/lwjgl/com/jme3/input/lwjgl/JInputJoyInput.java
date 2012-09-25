4b4ae178e58476b15505f7c0baf9ad49d52a1ac1
PSp..om <PSpeed42@gmail.com>
2012-09-25 06:31:25 +0000

    Added a basic way to have default remappings of joysticks that properly report their name.  It's a simple system using properties files on the classpath.  A core file has been added with at least one known remapping. Buttons and axes can be renamed but they can't yet be flipped.  If needed that can be added later.
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@9774 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/lwjgl/com/jme3/input/lwjgl/JInputJoyInput.java
+++ engine/src/lwjgl/com/jme3/input/lwjgl/JInputJoyInput.java
@@ -8,6 +8,7 @@ import com.jme3.input.JoyInput;
 import com.jme3.input.Joystick;
 import com.jme3.input.JoystickAxis;
 import com.jme3.input.JoystickButton;
+import com.jme3.input.JoystickCompatibilityMappings;
 import com.jme3.input.RawInputListener;
 import com.jme3.input.event.JoyAxisEvent;
 import com.jme3.input.event.JoyButtonEvent;
@@ -109,7 +110,7 @@ public class JInputJoyInput implements JoyInput {
                 if (id == Identifier.Axis.POV){
                     float x = 0, y = 0;
                     float v = e.getValue();
-                    
+ 
                     if (v == POV.CENTER){
                         x = 0; y = 0;
                     }else if (v == POV.DOWN){
@@ -212,8 +213,14 @@ public class JInputJoyInput implements JoyInput {
                 throw new IllegalArgumentException( "Component is not an axis:" + comp );
             }
 
+            String original = comp.getName();
+            String name = JoystickCompatibilityMappings.remapComponent( controller.getName(), original );
+            if( name != original ) {
+                logger.log(Level.INFO, "Remapped:" + original + " to:" + name);
+            }
+ 
             JoystickButton button = new DefaultJoystickButton( getInputManager(), this, getButtonCount(),
-                                                               comp.getName(), id.getName() );
+                                                               name, id.getName() );
             addButton(button);                                                               
             buttonIndex.put( comp, button );
         }
@@ -227,8 +234,14 @@ public class JInputJoyInput implements JoyInput {
                 throw new IllegalArgumentException( "Component is not an axis:" + comp );
             }
             
+            String original = comp.getName();
+            String name = JoystickCompatibilityMappings.remapComponent( controller.getName(), original );
+            if( name != original ) {
+                logger.log(Level.INFO, "Remapped:" + original + " to:" + name);
+            }
+            
             JoystickAxis axis = new DefaultJoystickAxis( getInputManager(), 
-                                                         this, getAxisCount(), comp.getName(), id.getName(),
+                                                         this, getAxisCount(), name, id.getName(),
                                                          comp.isAnalog(), comp.isRelative(), 
                                                          comp.getDeadZone() );
             addAxis(axis);                                                          
