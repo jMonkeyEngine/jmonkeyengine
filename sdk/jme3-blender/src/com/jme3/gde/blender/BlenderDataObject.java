527a7b53215879ef56e610a45f6fbf30e24b1700
nor..67 <normen667@gmail.com>
2013-01-30 01:33:35 +0000

    SDK: - fix BlenderDataObject righ-click properties
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@10235 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- sdk/jme3-blender/src/com/jme3/gde/blender/BlenderDataObject.java
+++ sdk/jme3-blender/src/com/jme3/gde/blender/BlenderDataObject.java
@@ -24,21 +24,21 @@ import org.openide.loaders.MultiFileLoader;
     @ActionReference(id =
     @ActionID(category = "jMonkeyPlatform", id = "com.jme3.gde.core.assets.actions.OpenModel"), path = "Loaders/application/blender/Actions", position = 20),
     @ActionReference(id =
-    @ActionID(category = "Edit", id = "org.openide.actions.CutAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 200, separatorBefore = 100),
+    @ActionID(category = "Edit", id = "org.openide.actions.CutAction"), path = "Loaders/application/blender/Actions", position = 200, separatorBefore = 100),
     @ActionReference(id =
-    @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 300, separatorAfter = 400),
+    @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"), path = "Loaders/application/blender/Actions", position = 300, separatorAfter = 400),
     @ActionReference(id =
-    @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 500),
+    @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"), path = "Loaders/application/blender/Actions", position = 500),
     @ActionReference(id =
-    @ActionID(category = "System", id = "org.openide.actions.RenameAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 600, separatorAfter = 700),
+    @ActionID(category = "System", id = "org.openide.actions.RenameAction"), path = "Loaders/application/blender/Actions", position = 600, separatorAfter = 700),
     @ActionReference(id =
-    @ActionID(category = "System", id = "org.openide.actions.SaveAsTemplateAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 800, separatorAfter = 900),
+    @ActionID(category = "System", id = "org.openide.actions.SaveAsTemplateAction"), path = "Loaders/application/blender/Actions", position = 800, separatorAfter = 900),
     @ActionReference(id =
-    @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 1000, separatorAfter = 1100),
+    @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"), path = "Loaders/application/blender/Actions", position = 1000, separatorAfter = 1100),
     @ActionReference(id =
-    @ActionID(category = "System", id = "org.openide.actions.ToolsAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 1200),
+    @ActionID(category = "System", id = "org.openide.actions.ToolsAction"), path = "Loaders/application/blender/Actions", position = 1200),
     @ActionReference(id =
-    @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"), path = "Loaders/application/jme3filterpostprocessor/Actions", position = 1300)})
+    @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"), path = "Loaders/application/blender/Actions", position = 1300)})
 public class BlenderDataObject extends SpatialAssetDataObject {
 
     public BlenderDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
