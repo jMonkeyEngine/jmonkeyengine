76b4306d10193fe8e6b20c83a9335eb18e45346e
rem..om <remy.bouquet@gmail.com>
2013-03-09 22:40:55 +0000

    ShaderNodesLoader : removed unused jogAmp dependency that slipped in....
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@10480 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core-plugins/com/jme3/material/plugins/ShaderNodeLoaderDelegate.java
+++ engine/src/core-plugins/com/jme3/material/plugins/ShaderNodeLoaderDelegate.java
@@ -51,7 +51,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import jogamp.common.os.elf.Shdr;
 
 /**
  * This class is here to be able to load shaderNodeDefinition from both the
