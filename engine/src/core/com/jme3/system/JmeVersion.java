e2b7ebaf76b384a03a8923dcca6dca4f1dc4f145
iwg..ic <iwgeric@gmail.com>
2013-11-03 03:28:24 +0000

    Bump engine version number to 3.0.3
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/branches/3.0final@10869 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- engine/src/core/com/jme3/system/JmeVersion.java
+++ engine/src/core/com/jme3/system/JmeVersion.java
@@ -32,5 +32,5 @@
 package com.jme3.system;
 
 public class JmeVersion {
-    public static final String FULL_NAME = "jMonkeyEngine 3.0.2";
+    public static final String FULL_NAME = "jMonkeyEngine 3.0.3";
 }
