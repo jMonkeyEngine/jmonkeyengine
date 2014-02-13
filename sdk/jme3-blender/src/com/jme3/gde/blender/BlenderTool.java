e645ff1c8d2a1eeccc7450aaa8ae7178654d6b84
nor..67 <normen667@gmail.com>
2014-02-13 18:11:19 +0000

    - use both blender versions in BlenderTool
    git-svn-id: https://jmonkeyengine.googlecode.com/svn/trunk@11042 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- sdk/jme3-blender/src/com/jme3/gde/blender/BlenderTool.java
+++ sdk/jme3-blender/src/com/jme3/gde/blender/BlenderTool.java
@@ -252,6 +252,9 @@ public class BlenderTool {
     private static File getBlenderSettingsFolder() {
         File blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.69", null, false);
         if (blender == null) {
+            blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.67", null, false);
+        }
+        if (blender == null) {
             DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message("Error finding Blender settings"));
             logger.log(Level.SEVERE, "Error finding Blender settings");
         }
