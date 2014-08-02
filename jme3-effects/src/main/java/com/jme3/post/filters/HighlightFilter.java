07b39e85e9ac1eba5882fbcd281716039e20eb6a
shamanDevel <muell.filter@gmail.com>
2014-08-02 14:52:16 +0200

    fixed indentation

--- jme3-effects/src/main/java/com/jme3/post/filters/HighlightFilter.java
+++ jme3-effects/src/main/java/com/jme3/post/filters/HighlightFilter.java
@@ -38,137 +38,134 @@ import com.jme3.export.JmeImporter;
 import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector2f;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.Renderer;
 import com.jme3.renderer.ViewPort;
 import com.jme3.renderer.queue.RenderQueue;
-import com.jme3.shader.VarType;
 import com.jme3.texture.Image;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
- * A filter that draws a highlighting shape around objects in the scene that have
- * the material parameter {@code Highlighted} set to true.
+ * A filter that draws a highlighting shape around objects in the scene that
+ * have the material parameter {@code Highlighted} set to true.
+ *
  * @author Sebastian Weiss
  */
 public class HighlightFilter extends Filter {
-	
-	public static final int MIN_BRUSH_SIZE = 1;
-	public static final int MAX_BRUSH_SIZE = 3;
-	
-	private Material drawHighlightMaterial;
-	private Pass preHighlightPass;
-	
-	private int size = MIN_BRUSH_SIZE;
-	private boolean debug = false;
-	private ColorRGBA highlightColor = ColorRGBA.BlackNoAlpha;
-	
-	private int screenWidth;
-    private int screenHeight;    
+
+    public static final int MIN_BRUSH_SIZE = 1;
+    public static final int MAX_BRUSH_SIZE = 3;
+
+    private Material drawHighlightMaterial;
+    private Pass preHighlightPass;
+
+    private int size = MIN_BRUSH_SIZE;
+    private boolean debug = false;
+    private ColorRGBA highlightColor = ColorRGBA.BlackNoAlpha;
+
+    private int screenWidth;
+    private int screenHeight;
     private RenderManager renderManager;
     private ViewPort viewPort;
     private AssetManager assetManager;
 
-	public HighlightFilter() {
-		super("HighlightFilter");
-	}
-	
-	@Override
-	protected void initFilter(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
-		this.renderManager = renderManager;
-        this.viewPort = vp;
-
-        this.assetManager = manager;             
-        screenWidth = w;
-        screenHeight = h;
-		
-		preHighlightPass = new Pass();
-        preHighlightPass.init(renderManager.getRenderer(), screenWidth, screenHeight, Image.Format.RGB8, Image.Format.Depth);
-		
-		drawHighlightMaterial = new Material(manager, "Common/MatDefs/Post/HighlightFinal.j3md");
-		drawHighlightMaterial.setTexture("HighlightTex", preHighlightPass.getRenderedTexture());
-		drawHighlightMaterial.setFloat("StepX", 1f/w);
-		drawHighlightMaterial.setFloat("StepY", 1f/h);	
-		
-		debugEnabled(debug);
-		setBrushSize(size);
-		setHighlightColor(highlightColor);
-	}
-
-	@Override
-	protected Material getMaterial() {
-		return drawHighlightMaterial;
-	}
-	
-	@Override
-    protected void postQueue(RenderQueue queue) {
-		renderManager.getRenderer().setBackgroundColor(ColorRGBA.BlackNoAlpha);            
-		renderManager.getRenderer().setFrameBuffer(preHighlightPass.getRenderFrameBuffer());
-		renderManager.getRenderer().clearBuffers(true, true, true);
-		renderManager.setForcedTechnique("Highlight");
-		renderManager.renderViewPortQueues(viewPort, false);         
-		renderManager.setForcedTechnique(null);
-		renderManager.getRenderer().setFrameBuffer(viewPort.getOutputFrameBuffer());
+    public HighlightFilter() {
+        super("HighlightFilter");
     }
 
-	@Override
-	protected void cleanUpFilter(Renderer r) {
-		preHighlightPass.cleanup(r);
-	}
-	
-	public void debugEnabled(boolean debug) {
-		this.debug = debug;
-		if (drawHighlightMaterial!=null) {
-			drawHighlightMaterial.setBoolean("Debug", debug);
-		}
-	}
-	
-	public void setBrushSize(int size) {
-		if (size<MIN_BRUSH_SIZE || size>MAX_BRUSH_SIZE) {
-			throw new IllegalArgumentException("brush size must be greater equals than "+MIN_BRUSH_SIZE
-				+" and smaller equals than "+MAX_BRUSH_SIZE);
-		}
-		this.size = size;
-		if (drawHighlightMaterial!=null) {
-			drawHighlightMaterial.setInt("Size", size);
-		}
-	}
-	
-	public int getBrushSize() {
-		return size;
-	}
-	
-	public void setHighlightColor(ColorRGBA col) {
-		if (col==null) {
-			throw new NullPointerException("color is null");
-		}
-		this.highlightColor = col;
-		if (drawHighlightMaterial!=null) {
-			drawHighlightMaterial.setColor("HighlightColor", col);
-		}
-	}
-	
-	public ColorRGBA getHighlightColor() {
-		return highlightColor;
-	}
-	
-	@Override
+    @Override
+    protected void initFilter(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
+        this.renderManager = renderManager;
+        this.viewPort = vp;
+
+        this.assetManager = manager;
+        screenWidth = w;
+        screenHeight = h;
+
+        preHighlightPass = new Pass();
+        preHighlightPass.init(renderManager.getRenderer(), screenWidth, screenHeight, Image.Format.RGB8, Image.Format.Depth);
+
+        drawHighlightMaterial = new Material(manager, "Common/MatDefs/Post/HighlightFinal.j3md");
+        drawHighlightMaterial.setTexture("HighlightTex", preHighlightPass.getRenderedTexture());
+        drawHighlightMaterial.setFloat("StepX", 1f / w);
+        drawHighlightMaterial.setFloat("StepY", 1f / h);
+
+        debugEnabled(debug);
+        setBrushSize(size);
+        setHighlightColor(highlightColor);
+    }
+
+    @Override
+    protected Material getMaterial() {
+        return drawHighlightMaterial;
+    }
+
+    @Override
+    protected void postQueue(RenderQueue queue) {
+        renderManager.getRenderer().setBackgroundColor(ColorRGBA.BlackNoAlpha);
+        renderManager.getRenderer().setFrameBuffer(preHighlightPass.getRenderFrameBuffer());
+        renderManager.getRenderer().clearBuffers(true, true, true);
+        renderManager.setForcedTechnique("Highlight");
+        renderManager.renderViewPortQueues(viewPort, false);
+        renderManager.setForcedTechnique(null);
+        renderManager.getRenderer().setFrameBuffer(viewPort.getOutputFrameBuffer());
+    }
+
+    @Override
+    protected void cleanUpFilter(Renderer r) {
+        preHighlightPass.cleanup(r);
+    }
+
+    public void debugEnabled(boolean debug) {
+        this.debug = debug;
+        if (drawHighlightMaterial != null) {
+            drawHighlightMaterial.setBoolean("Debug", debug);
+        }
+    }
+
+    public void setBrushSize(int size) {
+        if (size < MIN_BRUSH_SIZE || size > MAX_BRUSH_SIZE) {
+            throw new IllegalArgumentException("brush size must be greater equals than " + MIN_BRUSH_SIZE
+                    + " and smaller equals than " + MAX_BRUSH_SIZE);
+        }
+        this.size = size;
+        if (drawHighlightMaterial != null) {
+            drawHighlightMaterial.setInt("Size", size);
+        }
+    }
+
+    public int getBrushSize() {
+        return size;
+    }
+
+    public void setHighlightColor(ColorRGBA col) {
+        if (col == null) {
+            throw new NullPointerException("color is null");
+        }
+        this.highlightColor = col;
+        if (drawHighlightMaterial != null) {
+            drawHighlightMaterial.setColor("HighlightColor", col);
+        }
+    }
+
+    public ColorRGBA getHighlightColor() {
+        return highlightColor;
+    }
+
+    @Override
     public void write(JmeExporter ex) throws IOException {
         super.write(ex);
         OutputCapsule oc = ex.getCapsule(this);
-		oc.write(size, "brushSize", MIN_BRUSH_SIZE);
-		oc.write(highlightColor, "highlightColor", ColorRGBA.BlackNoAlpha);
+        oc.write(size, "brushSize", MIN_BRUSH_SIZE);
+        oc.write(highlightColor, "highlightColor", ColorRGBA.BlackNoAlpha);
     }
 
     @Override
     public void read(JmeImporter im) throws IOException {
         super.read(im);
         InputCapsule ic = im.getCapsule(this);
-		size = ic.readInt("brushSize", MIN_BRUSH_SIZE);
-		highlightColor = (ColorRGBA) ic.readSavable("highlightColor", ColorRGBA.BlackNoAlpha);
+        size = ic.readInt("brushSize", MIN_BRUSH_SIZE);
+        highlightColor = (ColorRGBA) ic.readSavable("highlightColor", ColorRGBA.BlackNoAlpha);
     }
 }
--- jme3-examples/src/main/java/jme3test/post/TestHighlighting.java
+++ jme3-examples/src/main/java/jme3test/post/TestHighlighting.java
@@ -29,7 +29,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package jme3test.post;
 
 import com.jme3.app.SimpleApplication;
@@ -58,55 +57,56 @@ import java.io.File;
 import jme3test.scene.*;
 
 /**
- * Tests the highlighting of objects in a simple scene.
- * Scene copied from TestSceneLoading.
+ * Tests the highlighting of objects in a simple scene. Scene copied from
+ * TestSceneLoading.
+ *
  * @author Sebastian Weiss
  */
 public class TestHighlighting extends SimpleApplication implements ActionListener {
 
-    private static boolean useHttp = false;
-	
+	private static boolean useHttp = false;
+
 	private Spatial scene;
 	private Geometry highlighted;
 	private boolean debug;
 	private HighlightFilter filter;
 
-    public static void main(String[] args) {
-     
-        TestHighlighting app = new TestHighlighting();
-        app.start();
-    }
+	public static void main(String[] args) {
 
-    public void simpleInitApp() {
-        this.flyCam.setMoveSpeed(10);
+		TestHighlighting app = new TestHighlighting();
+		app.start();
+	}
+
+	public void simpleInitApp() {
+		this.flyCam.setMoveSpeed(10);
 		this.flyCam.setDragToRotate(true);
-		
+
 		//add highlighting filter
-		FilterPostProcessor fpp=new FilterPostProcessor(assetManager);
-       // fpp.setNumSamples(4);
-        int numSamples = getContext().getSettings().getSamples();
-        if( numSamples > 0 ) {
-            fpp.setNumSamples(numSamples); 
-        }
+		FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+		// fpp.setNumSamples(4);
+		int numSamples = getContext().getSettings().getSamples();
+		if (numSamples > 0) {
+			fpp.setNumSamples(numSamples);
+		}
 		filter = new HighlightFilter();
 		fpp.addFilter(filter);
 		viewPort.addProcessor(fpp);
 
-        // load sky
-        rootNode.attachChild(SkyFactory.createSky(assetManager, "Textures/Sky/Bright/BrightSky.dds", false));
+		// load sky
+		rootNode.attachChild(SkyFactory.createSky(assetManager, "Textures/Sky/Bright/BrightSky.dds", false));
 
-        File file = new File("wildhouse.zip");
-        if (!file.exists()) {
-            useHttp = true;
-        }
+		File file = new File("wildhouse.zip");
+		if (!file.exists()) {
+			useHttp = true;
+		}
         // create the geometry and attach it
-        // load the level from zip or http zip
-        if (useHttp) {
-            assetManager.registerLocator("http://jmonkeyengine.googlecode.com/files/wildhouse.zip", HttpZipLocator.class);
-        } else {
-            assetManager.registerLocator("wildhouse.zip", ZipLocator.class);
-        }
-        scene = assetManager.loadModel("main.scene");
+		// load the level from zip or http zip
+		if (useHttp) {
+			assetManager.registerLocator("http://jmonkeyengine.googlecode.com/files/wildhouse.zip", HttpZipLocator.class);
+		} else {
+			assetManager.registerLocator("wildhouse.zip", ZipLocator.class);
+		}
+		scene = assetManager.loadModel("main.scene");
 //		scene = new Node();
 //        Box box1 = new Box(1,1,1);
 //        Geometry blue = new Geometry("Box", box1);
@@ -117,16 +117,16 @@ public class TestHighlighting extends SimpleApplication implements ActionListene
 //        blue.setMaterial(mat1);
 //		((Node) scene).attachChild(blue);
 
-        AmbientLight al = new AmbientLight();
-        scene.addLight(al);
+		AmbientLight al = new AmbientLight();
+		scene.addLight(al);
 
-        DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(0.69077975f, -0.6277887f, -0.35875428f).normalizeLocal());
-        sun.setColor(ColorRGBA.White.clone().multLocal(2));
-        scene.addLight(sun);
+		DirectionalLight sun = new DirectionalLight();
+		sun.setDirection(new Vector3f(0.69077975f, -0.6277887f, -0.35875428f).normalizeLocal());
+		sun.setColor(ColorRGBA.White.clone().multLocal(2));
+		scene.addLight(sun);
 
-        rootNode.attachChild(scene);
-		
+		rootNode.attachChild(scene);
+
 		//initialize input
 		inputManager.addMapping("clicked", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
 		inputManager.addMapping("debug", new KeyTrigger(KeyInput.KEY_SPACE));
@@ -147,7 +147,7 @@ public class TestHighlighting extends SimpleApplication implements ActionListene
 		System.out.println(" Return: toggle highlight filter");
 		System.out.println(" c: change highlight color");
 		System.out.println("---------------------------------");
-    }
+	}
 
 	public void onAction(String name, boolean isPressed, float tpf) {
 		if (!isPressed) {
@@ -156,43 +156,38 @@ public class TestHighlighting extends SimpleApplication implements ActionListene
 		if ("debug".equals(name)) {
 			debug = !debug;
 			filter.debugEnabled(debug);
-		}
-		else if ("clicked".equals(name)) {
+		} else if ("clicked".equals(name)) {
 			//highlight selected element
-			Vector3f origin    = cam.getWorldCoordinates(inputManager.getCursorPosition(), 0.0f);
+			Vector3f origin = cam.getWorldCoordinates(inputManager.getCursorPosition(), 0.0f);
 			Vector3f direction = cam.getWorldCoordinates(inputManager.getCursorPosition(), 0.3f);
 			direction.subtractLocal(origin).normalizeLocal();
 
 			Ray ray = new Ray(origin, direction);
 			CollisionResults results = new CollisionResults();
 			scene.collideWith(ray, results);
-			
-			if (highlighted!=null) {
+
+			if (highlighted != null) {
 				highlighted.getMaterial().setBoolean("Highlighted", false);
 				highlighted = null;
 			}
-			
+
 			CollisionResult res = results.getClosestCollision();
-			if (res!=null) {
+			if (res != null) {
 				highlighted = res.getGeometry();
 				highlighted.getMaterial().setBoolean("Highlighted", true);
 				System.out.println(highlighted + " selected");
 			} else {
 				System.out.println("nothing selected");
 			}
-		}
-		else if ("+Brush".equals(name)) {
-			filter.setBrushSize(Math.min(HighlightFilter.MAX_BRUSH_SIZE, filter.getBrushSize()+1));
-			System.out.println("brush size changed to "+filter.getBrushSize());
-		}
-		else if ("-Brush".equals(name)) {
-			filter.setBrushSize(Math.max(HighlightFilter.MIN_BRUSH_SIZE, filter.getBrushSize()-1));
-			System.out.println("brush size changed to "+filter.getBrushSize());
-		}
-		else if ("enabled".equals(name)) {
+		} else if ("+Brush".equals(name)) {
+			filter.setBrushSize(Math.min(HighlightFilter.MAX_BRUSH_SIZE, filter.getBrushSize() + 1));
+			System.out.println("brush size changed to " + filter.getBrushSize());
+		} else if ("-Brush".equals(name)) {
+			filter.setBrushSize(Math.max(HighlightFilter.MIN_BRUSH_SIZE, filter.getBrushSize() - 1));
+			System.out.println("brush size changed to " + filter.getBrushSize());
+		} else if ("enabled".equals(name)) {
 			filter.setEnabled(!filter.isEnabled());
-		}
-		else if ("color".equals(name)) {
+		} else if ("color".equals(name)) {
 			filter.setHighlightColor(ColorRGBA.randomColor());
 		}
 	}
