947d587f7b835956112db5336b38dc8941bdd954
Normen Hansen <normen667@gmail.com>
2015-01-22 22:05:39 +0100

    - Move Texture Editor layer registration to Annotations

--- sdk/jme3-texture-editor/src/com/jme3/gde/textureeditor/JmeTextureDataObject.java
+++ sdk/jme3-texture-editor/src/com/jme3/gde/textureeditor/JmeTextureDataObject.java
@@ -5,8 +5,13 @@
 package com.jme3.gde.textureeditor;
 
 import java.io.IOException;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.MIMEResolver;
 import org.openide.loaders.DataNode;
+import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
@@ -14,6 +19,32 @@ import org.openide.nodes.Node;
 import org.openide.nodes.Children;
 import org.openide.util.Lookup;
 
+@MIMEResolver.ExtensionRegistration(
+        displayName = "jME3 Texture",
+        mimeType = "image/x-jmetexture",
+        extension = {"png", "bmp", "jpg", "PNG", "BMP", "JPG", "jpeg", "JPEG", "tga", "TGA"}
+)
+@DataObject.Registration(displayName = "jME3 Texture", mimeType = "image/x-jmetexture", iconBase = "com/jme3/gde/textureeditor/Computer_File_068.gif")
+@ActionReferences(value = {
+    @ActionReference(id =
+    @ActionID(category = "JME3", id = "com.jme3.gde.textureeditor.OpenTexture"), path = "Loaders/image/x-jmetexture/Actions", position = 0),
+    @ActionReference(id =
+    @ActionID(category = "Edit", id = "org.openide.actions.CutAction"), path = "Loaders/image/x-jmetexture/Actions", position = 200, separatorBefore = 100),
+    @ActionReference(id =
+    @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"), path = "Loaders/image/x-jmetexture/Actions", position = 300, separatorAfter = 400),
+    @ActionReference(id =
+    @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"), path = "Loaders/image/x-jmetexture/Actions", position = 500),
+    @ActionReference(id =
+    @ActionID(category = "System", id = "org.openide.actions.RenameAction"), path = "Loaders/image/x-jmetexture/Actions", position = 600, separatorAfter = 700),
+    @ActionReference(id =
+    @ActionID(category = "System", id = "org.openide.actions.SaveAsTemplateAction"), path = "Loaders/image/x-jmetexture/Actions", position = 800, separatorAfter = 900),
+    @ActionReference(id =
+    @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"), path = "Loaders/image/x-jmetexture/Actions", position = 1000, separatorAfter = 1100),
+    @ActionReference(id =
+    @ActionID(category = "System", id = "org.openide.actions.ToolsAction"), path = "Loaders/image/x-jmetexture/Actions", position = 1200),
+    @ActionReference(id =
+    @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"), path = "Loaders/image/x-jmetexture/Actions", position = 1300)
+})
 public class JmeTextureDataObject extends MultiDataObject {
 
     public JmeTextureDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
---
+++ sdk/jme3-texture-editor/src/com/jme3/gde/textureeditor/package-info.java
@@ -0,0 +1,9 @@
+@ActionReferences(value = {
+    @ActionReference(id =
+    @ActionID(category = "JME3", id = "com.jme3.gde.textureeditor.OpenTexture"), path = "Loaders/image/png-gif-jpeg-bmp/Actions", position = 0),
+})
+package com.jme3.gde.textureeditor;
+
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
