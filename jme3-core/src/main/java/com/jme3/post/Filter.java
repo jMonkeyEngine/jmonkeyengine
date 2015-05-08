3f3ef99b8693cfab8ebc2e74c766ddb504ca4a16
Nehon <remy.bouquet@gmail.com>
2015-05-08 11:18:27 +0200

    Fixed an issue where AA was not taken into account when using SSAO filter

--- jme3-core/src/main/java/com/jme3/post/Filter.java
+++ jme3-core/src/main/java/com/jme3/post/Filter.java
@@ -233,11 +233,12 @@ public abstract class Filter implements Savable {
      * @param vp the viewport
      * @param w the width
      * @param h the height
+     * @param numSamples the number of samples for anti aliasing
      */
-    protected final void init(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
+    protected final void init(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h, int numSamples) {
         //  cleanup(renderManager.getRenderer());
         defaultPass = new Pass();
-        defaultPass.init(renderManager.getRenderer(), w, h, getDefaultPassTextureFormat(), getDefaultPassDepthFormat());
+        defaultPass.init(renderManager.getRenderer(), w, h, getDefaultPassTextureFormat(), getDefaultPassDepthFormat(), numSamples);
         initFilter(manager, renderManager, vp, w, h);
     }
 
