e14c6467ffd1285eb6a818fde3c6ac1dae30027d
Kirill Vainer <shadowislord@gmail.com>
2015-08-15 13:09:38 -0400

    sdk: update to blender 2.75a

--- sdk/jme3-blender/src/com/jme3/gde/blender/BlenderTool.java
+++ sdk/jme3-blender/src/com/jme3/gde/blender/BlenderTool.java
@@ -251,9 +251,9 @@ public class BlenderTool {
     }
 
     private static File getBlenderSettingsFolder() {
-        File blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.69", null, false);
+        File blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.75", null, false);
         if (blender == null) {
-            blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.67", null, false);
+            blender = InstalledFileLocator.getDefault().locate(getBlenderOsPath() + "/2.69", null, false);
         }
         if (blender == null) {
             DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message("Error finding Blender settings"));
