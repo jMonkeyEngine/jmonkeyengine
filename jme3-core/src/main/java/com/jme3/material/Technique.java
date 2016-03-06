152a7638cde40e70afa348b3c42b058d2fb937ea
Kirill Vainer <shadowislord@gmail.com>
2016-03-05 19:30:28 -0500

    material: fix sort id unit test failure

--- jme3-core/src/main/java/com/jme3/material/Technique.java
+++ jme3-core/src/main/java/com/jme3/material/Technique.java
@@ -184,9 +184,9 @@ public final class Technique {
      * @return the sort ID for this technique instance.
      */
     public int getSortId() {
-       int hash = 17;
-       hash = hash * 23 + def.getSortId();
-       hash = hash * 23 + dynamicDefines.hashCode();
-       return hash;
+        int hash = 17;
+        hash = hash * 23 + def.getSortId();
+        hash = hash * 23 + paramDefines.hashCode();
+        return hash;
     }
 }
