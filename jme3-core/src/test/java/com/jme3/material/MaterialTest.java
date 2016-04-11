8d4962d11f8df8d438752cfaa966bde32ea0650e
Kirill Vainer <shadowislord@gmail.com>
2016-04-10 23:46:41 -0400

    fix failing unit test

--- jme3-core/src/test/java/com/jme3/material/MaterialTest.java
+++ jme3-core/src/test/java/com/jme3/material/MaterialTest.java
@@ -114,19 +114,19 @@ public class MaterialTest {
     @Test
     public void testSelectNamedTechnique_GLSL150Cap() {
         supportGlsl(150);
-        material("Common/MatDefs/Misc/Particle.j3md");
+        material("Common/MatDefs/Light/Lighting.j3md");
 
-        material.selectTechnique("SoftParticles", renderManager);
+        material.selectTechnique("PostShadow", renderManager);
 
-        checkRequiredCaps(Caps.GLSL100, Caps.GLSL150);
+        checkRequiredCaps(Caps.GLSL150);
     }
 
     @Test
     public void testSelectNamedTechnique_GLSL100Cap() {
         supportGlsl(100);
-        material("Common/MatDefs/Misc/Particle.j3md");
+        material("Common/MatDefs/Light/Lighting.j3md");
 
-        material.selectTechnique("SoftParticles", renderManager);
+        material.selectTechnique("PostShadow", renderManager);
 
         checkRequiredCaps(Caps.GLSL100);
     }
