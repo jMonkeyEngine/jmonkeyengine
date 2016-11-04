bc86161831ab30cc87022b6f9ea3812e2360f506
Nehon <remy.bouquet@gmail.com>
2016-11-04 16:59:07 +0100

    fixed some javadoc in AbstractShadowRenderer

--- jme3-core/src/main/java/com/jme3/shadow/AbstractShadowRenderer.java
+++ jme3-core/src/main/java/com/jme3/shadow/AbstractShadowRenderer.java
@@ -725,7 +725,7 @@ public abstract class AbstractShadowRenderer implements SceneProcessor, Savable 
     }
 
     /**
-     * Sets the shadow edges thickness. default is 1, setting it to lower values
+     * Sets the shadow edges thickness. default is 10, setting it to lower values
      * can help to reduce the jagged effect of the shadow edges
      *
      * @param edgesThickness
