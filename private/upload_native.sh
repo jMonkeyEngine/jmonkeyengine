19c5c2df248abf5d4bc06413533bb04e86e6633e
Kirill Vainer <shadowislord@gmail.com>
2016-11-26 00:38:15 -0500

    travis: set key permissions

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -6,6 +6,7 @@ if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.name "travis-ci"
     ./gradlew --no-daemon -PbuildNativeProjects=true :jme3-bullet-native:assemble
     openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    chmod 600 "$HOME/.ssh/id_rsa"
     git checkout -q "$TRAVIS_BRANCH"
     git add -- jme3-bullet-native/libs/native/
     git commit -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
