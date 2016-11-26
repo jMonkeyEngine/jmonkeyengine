8b2b57f01cacf0f9df6aa703f2b2ddd773e7aa07
Kirill Vainer <shadowislord@gmail.com>
2016-11-26 00:10:30 -0500

    travis: test native upload

---
+++ private/upload_native.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
+if [ "$NATIVE_CHANGES" != "" ]; then
+    ./gradlew --no-daemon -PbuildNativeProjects=true :jme3-bullet-native:assemble
+    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    git checkout -q "$TRAVIS_BRANCH"
+    git add -- jme3-bullet-native/libs/native/
+    git commit -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
+    git pull -q --rebase
+    git push git@github.com:jMonkeyEngine/jmonkeyengine.git
+fi
