e268c3b9fe6a2ccbac355e84656d1731a6baacf5
travis-ci <travis-ci>
2016-11-27 14:18:21 -0500

    upload_native: more debugging

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
-set -e
-NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
-if [ "$NATIVE_CHANGES" != "" ]; then
+#!/bin/bash
+set -euo pipefail
+# NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
+# if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.email "travis-ci"
     git config --global user.name "travis-ci"
-    ./gradlew -PbuildNativeProjects=true :jme3-bullet-native:assemble
-    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
-    chmod 600 "$HOME/.ssh/id_rsa"
-    git checkout -q "$TRAVIS_BRANCH"
-    git add -- jme3-bullet-native/libs/native/
-    git commit -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
+    # ./gradlew -PbuildNativeProjects=true :jme3-bullet-native:assemble
+    # openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    # chmod 600 "$HOME/.ssh/id_rsa"
+    md5sum jme3-bullet-native/build/libs/bulletjme/shared/linux32/libbulletjme.so jme3-bullet-native/libs/native/linux/x86/libbulletjme.so
+    git checkout "$TRAVIS_BRANCH"
+    git add -v -- jme3-bullet-native/libs/native/
+    git commit -v -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
     git pull -q --rebase
     git push git@github.com:jMonkeyEngine/jmonkeyengine.git
-fi
+# fi
