da8b53d0a2c39e5525840fcde8242206465ca27a
Kirill Vainer <shadowislord@gmail.com>
2016-11-27 15:16:28 -0500

    upload_native: always assemble native

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -4,7 +4,6 @@ NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-nati
 if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.email "travis-ci"
     git config --global user.name "travis-ci"
-    ./gradlew -PbuildNativeProjects=true :jme3-bullet-native:assemble
     openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
     chmod 600 "$HOME/.ssh/id_rsa"
     git checkout "$TRAVIS_BRANCH"
