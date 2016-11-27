0cfb97359f892fb0f3b305cb3ebb69d868ed2a58
Kirill Vainer <shadowislord@gmail.com>
2016-11-27 11:46:03 -0500

    Revert "travis: test mac native upload (2)"
    This reverts commit 6e352bbf8a4e0df69f2dd4b1c5329237e504ed47.

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 set -e
-# NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
+NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
 # if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.email "travis-ci"
     git config --global user.name "travis-ci"
-    # ./gradlew --no-daemon -PbuildNativeProjects=true :jme3-bullet-native:assemble
-    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_ecdsa" -d
-    chmod 600 "$HOME/.ssh/id_ecdsa"
-    ls -l "$HOME/.ssh/id_ecdsa"
+    ./gradlew --no-daemon -PbuildNativeProjects=true :jme3-bullet-native:assemble
+    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    chmod 600 "$HOME/.ssh/id_rsa"
     ssh -v -T git@github.com
     # git checkout -q "$TRAVIS_BRANCH"
     # git add -- jme3-bullet-native/libs/native/
