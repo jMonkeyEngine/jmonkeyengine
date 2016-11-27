dd812b17c56ba2cd68cb111ceae46ea0f1def995
Kirill Vainer <shadowislord@gmail.com>
2016-11-27 14:24:31 -0500

    upload_native: uncomment lines

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.email "travis-ci"
     git config --global user.name "travis-ci"
-    # ./gradlew -PbuildNativeProjects=true :jme3-bullet-native:assemble
-    # openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
-    # chmod 600 "$HOME/.ssh/id_rsa"
+    ./gradlew -PbuildNativeProjects=true :jme3-bullet-native:assemble
+    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    chmod 600 "$HOME/.ssh/id_rsa"
     md5sum jme3-bullet-native/build/libs/bulletjme/shared/linux32/libbulletjme.so jme3-bullet-native/libs/native/linux/x86/libbulletjme.so
     git checkout "$TRAVIS_BRANCH"
     git add -v -- jme3-bullet-native/libs/native/
