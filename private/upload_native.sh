1e15fe0f4427acf58dd693004c0b03dee3137769
Kirill Vainer <shadowislord@gmail.com>
2016-12-10 13:53:46 -0500

    bullet: test mac native upload

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
+echo Running upload native ...
 # NATIVE_CHANGES="$(git diff-tree --name-only "$TRAVIS_COMMIT" -- jme3-bullet-native/)"
 # if [ "$NATIVE_CHANGES" != "" ]; then
     git config --global user.email "travis-ci"
     git config --global user.name "travis-ci"
-    openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
-    chmod 600 "$HOME/.ssh/id_rsa"
+    # openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
+    # chmod 600 "$HOME/.ssh/id_rsa"
     ls jme3-bullet-native/build/libs/bulletjme/shared/
     md5 -r jme3-bullet-native/libs/native/osx/x86/libbulletjme.dylib jme3-bullet-native/build/libs/bulletjme/shared/mac32/libbulletjme.dylib
     git checkout "$TRAVIS_BRANCH"
