ea4920d3056938c3318c4e1a346e9822dba53029
MeFisto94 <MeFisto94@users.noreply.github.com>
2017-01-19 22:50:49 +0100

    Try to debug the native build issues

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -9,14 +9,19 @@ if [ "$NATIVE_CHANGES" != "" ]; then
     echo " - Decrypting private key"
     openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
     chmod 600 "$HOME/.ssh/id_rsa"
-    # ls jme3-bullet-native/build/libs/bulletjme/shared/
+    git status -v
+    ls -lsh jme3-bullet-native/build/libs/bulletjme/shared/
     # md5 -r jme3-bullet-native/libs/native/osx/x86/libbulletjme.dylib jme3-bullet-native/build/libs/bulletjme/shared/mac32/libbulletjme.dylib
     echo " - Pushing natives onto branch $TRAVIS_BRANCH"
     git checkout "$TRAVIS_BRANCH"
+    git status -v
     git add -v -- jme3-bullet-native/libs/native/
+    git status -v
     git commit -v -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
+    git status -v
     git pull -q --rebase
-    git push git@github.com:jMonkeyEngine/jmonkeyengine.git
+    git status -v
+    #git push git@github.com:jMonkeyEngine/jmonkeyengine.git
 else
     echo No native changes.
 fi
