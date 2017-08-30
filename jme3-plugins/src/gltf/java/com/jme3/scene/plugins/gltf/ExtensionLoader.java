716690690803c12d407dd59f4a85528b2315b7ad
Nehon <remy.bouquet@gmail.com>
2017-08-31 00:40:00 +0200

    Added support for loading "extras" from gltf files. Added some documentation

--- jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/ExtensionLoader.java
+++ jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/ExtensionLoader.java
@@ -3,10 +3,22 @@ package com.jme3.scene.plugins.gltf;
 import com.google.gson.JsonElement;
 
 /**
+ * Base Interface for extension loading implementation.
+ *
  * Created by Nehon on 20/08/2017.
  */
 public interface ExtensionLoader {
 
-    Object handleExtension(GltfLoader loader, JsonElement parent, JsonElement extension, Object input);
+    /**
+     * Implement this methods to handle a gltf extension reading
+     *
+     * @param loader     the GltfLoader with all the data structure.
+     * @param parentName the name of the element being read
+     * @param parent     the element being read
+     * @param extension  the content of the extension found in the element being read
+     * @param input      an object containing already loaded data from the element, this is most probably a JME object
+     * @return An object of the same type as input, containing the data from the input object and the eventual additional data read from the extension
+     */
+    Object handleExtension(GltfLoader loader, String parentName, JsonElement parent, JsonElement extension, Object input);
 
 }
