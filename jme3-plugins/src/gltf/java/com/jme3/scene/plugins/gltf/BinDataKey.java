b7acba62a19f754700a48d049693a030e1c50b15
Nehon <remy.bouquet@gmail.com>
2017-09-27 10:00:49 +0200

    glTF: properly close opened stream and avoid caching them. Fixed an issue when an animation track didn't have any translation or scale entries

--- jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/BinDataKey.java
+++ jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/BinDataKey.java
@@ -1,6 +1,7 @@
 package com.jme3.scene.plugins.gltf;
 
 import com.jme3.asset.AssetKey;
+import com.jme3.asset.cache.AssetCache;
 
 /**
  * Created by Nehon on 09/09/2017.
@@ -8,5 +9,10 @@ import com.jme3.asset.AssetKey;
 class BinDataKey extends AssetKey<Object> {
     public BinDataKey(String name) {
         super(name);
+    }
+
+    @Override
+    public Class<? extends AssetCache> getCacheType() {
+        return null;
     }
 }
