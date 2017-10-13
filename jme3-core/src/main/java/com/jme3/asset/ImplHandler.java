45f31d7b0981599cb701c4b8c76c1b5ff3fbcd2e
javasabr <javasabr@gmail.com>
2017-10-13 08:45:33 +0300

    simplified version.

--- jme3-core/src/main/java/com/jme3/asset/ImplHandler.java
+++ jme3-core/src/main/java/com/jme3/asset/ImplHandler.java
@@ -280,9 +280,8 @@ final class ImplHandler {
         // Synchronized access must be used for any ops on classToLoaderMap
         // Find the loader ImplThreadLocal for this class
         synchronized (classToLoaderMap){
-            ImplThreadLocal local = classToLoaderMap.get(loaderType);
             // Remove it from the class->loader map
-            classToLoaderMap.remove(loaderType);
+            ImplThreadLocal local = classToLoaderMap.remove(loaderType);
             if (local == null) return;
             // Remove it from the extension->loader map
             for (String extension : local.getExtensions()){
