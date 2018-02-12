932a22e7373ea132839243d86e60723d08dee023
javasabr <javasabr@gmail.com>
2018-02-12 09:51:58 +0300

    refactored and upgraed lwjgl3 module.

--- jme3-lwjgl3/src/main/java/com/jme3/audio/lwjgl/LwjglAL.java
+++ jme3-lwjgl3/src/main/java/com/jme3/audio/lwjgl/LwjglAL.java
@@ -31,8 +31,6 @@
  */package com.jme3.audio.lwjgl;
 
 import com.jme3.audio.openal.AL;
-import com.jme3.system.NativeLibraryLoader;
-import com.jme3.system.Platform;
 import org.lwjgl.openal.AL10;
 import org.lwjgl.openal.AL11;
 
@@ -40,101 +38,123 @@ import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
+/**
+ * The LWJGL implementation of {@link AL}.
+ */
 public final class LwjglAL implements AL {
 
     public LwjglAL() {
     }
 
-    public String alGetString(int parameter) {
+    @Override
+    public String alGetString(final int parameter) {
         return AL10.alGetString(parameter);
     }
 
+    @Override
     public int alGenSources() {
         return AL10.alGenSources();
     }
 
+    @Override
     public int alGetError() {
         return AL10.alGetError();
     }
 
-    public void alDeleteSources(int numSources, IntBuffer sources) {
+    @Override
+    public void alDeleteSources(final int numSources, final IntBuffer sources) {
         if (sources.position() != 0) throw new AssertionError();
         if (sources.limit() != numSources) throw new AssertionError();
         AL10.alDeleteSources(sources);
     }
 
-    public void alGenBuffers(int numBuffers, IntBuffer buffers) {
+    @Override
+    public void alGenBuffers(final int numBuffers, final IntBuffer buffers) {
         if (buffers.position() != 0) throw new AssertionError();
         if (buffers.limit() != numBuffers) throw new AssertionError();
         AL10.alGenBuffers(buffers);
     }
 
-    public void alDeleteBuffers(int numBuffers, IntBuffer buffers) {
+    @Override
+    public void alDeleteBuffers(final int numBuffers, final IntBuffer buffers) {
         if (buffers.position() != 0) throw new AssertionError();
         if (buffers.limit() != numBuffers) throw new AssertionError();
         AL10.alDeleteBuffers(buffers);
     }
 
-    public void alSourceStop(int source) {
+    @Override
+    public void alSourceStop(final int source) {
         AL10.alSourceStop(source);
     }
 
-    public void alSourcei(int source, int param, int value) {
+    @Override
+    public void alSourcei(final int source, final int param, final int value) {
         AL10.alSourcei(source, param, value);
     }
 
-    public void alBufferData(int buffer, int format, ByteBuffer data, int size, int frequency) {
+    @Override
+    public void alBufferData(final int buffer, final int format, final ByteBuffer data, final int size, final int frequency) {
         if (data.position() != 0) throw new AssertionError();
         if (data.limit() != size) throw new AssertionError();
         AL10.alBufferData(buffer, format, data, frequency);
     }
 
-    public void alSourcePlay(int source) {
+    @Override
+    public void alSourcePlay(final int source) {
         AL10.alSourcePlay(source);
     }
 
-    public void alSourcePause(int source) {
+    @Override
+    public void alSourcePause(final int source) {
         AL10.alSourcePause(source);
     }
 
-    public void alSourcef(int source, int param, float value) {
+    @Override
+    public void alSourcef(final int source, final int param, final float value) {
         AL10.alSourcef(source, param, value);
     }
 
-    public void alSource3f(int source, int param, float value1, float value2, float value3) {
+    @Override
+    public void alSource3f(final int source, final int param, final float value1, final float value2, final float value3) {
         AL10.alSource3f(source, param, value1, value2, value3);
     }
 
-    public int alGetSourcei(int source, int param) {
+    @Override
+    public int alGetSourcei(final int source, final int param) {
         return AL10.alGetSourcei(source, param);
     }
 
-    public void alSourceUnqueueBuffers(int source, int numBuffers, IntBuffer buffers) {
+    @Override
+    public void alSourceUnqueueBuffers(final int source, final int numBuffers, final IntBuffer buffers) {
         if (buffers.position() != 0) throw new AssertionError();
         if (buffers.limit() != numBuffers) throw new AssertionError();
         AL10.alSourceUnqueueBuffers(source, buffers);
     }
 
-    public void alSourceQueueBuffers(int source, int numBuffers, IntBuffer buffers) {
+    @Override
+    public void alSourceQueueBuffers(final int source, final int numBuffers, final IntBuffer buffers) {
         if (buffers.position() != 0) throw new AssertionError();
         if (buffers.limit() != numBuffers) throw new AssertionError();
         AL10.alSourceQueueBuffers(source, buffers);
     }
 
-    public void alListener(int param, FloatBuffer data) {
+    @Override
+    public void alListener(final int param, final FloatBuffer data) {
         AL10.alListenerfv(param, data);
     }
 
-    public void alListenerf(int param, float value) {
+    @Override
+    public void alListenerf(final int param, final float value) {
         AL10.alListenerf(param, value);
     }
 
-    public void alListener3f(int param, float value1, float value2, float value3) {
+    @Override
+    public void alListener3f(final int param, final float value1, final float value2, final float value3) {
         AL10.alListener3f(param, value1, value2, value3);
     }
 
-    public void alSource3i(int source, int param, int value1, int value2, int value3) {
+    @Override
+    public void alSource3i(final int source, final int param, final int value1, final int value2, final int value3) {
         AL11.alSource3i(source, param, value1, value2, value3);
     }
-
 }
--- jme3-lwjgl3/src/main/java/com/jme3/audio/lwjgl/LwjglALC.java
+++ jme3-lwjgl3/src/main/java/com/jme3/audio/lwjgl/LwjglALC.java
@@ -31,19 +31,27 @@
  */
 package com.jme3.audio.lwjgl;
 
+import org.lwjgl.openal.*;
+
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import org.lwjgl.openal.AL;
-import org.lwjgl.openal.ALC;
-import org.lwjgl.openal.ALC10;
-import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.openal.SOFTPauseDevice;
 
+/**
+ * The LWJGL implementation of {@link com.jme3.audio.openal.ALC}.
+ */
 public class LwjglALC implements com.jme3.audio.openal.ALC {
 
+    /**
+     * The device id.
+     */
     private long device;
+
+    /**
+     * The context id.
+     */
     private long context;
 
+    @Override
     public void createALC() {
         device = ALC10.alcOpenDevice((ByteBuffer) null);
         ALCCapabilities deviceCaps = ALC.createCapabilities(device);
@@ -52,6 +60,7 @@ public class LwjglALC implements com.jme3.audio.openal.ALC {
         AL.createCapabilities(deviceCaps);
     }
 
+    @Override
     public void destroyALC() {
         if (context != 0) {
             ALC10.alcDestroyContext(context);
@@ -64,18 +73,22 @@ public class LwjglALC implements com.jme3.audio.openal.ALC {
         }
     }
 
+    @Override
     public boolean isCreated() {
         return context != 0;
     }
 
+    @Override
     public String alcGetString(final int parameter) {
         return ALC10.alcGetString(device, parameter);
     }
 
+    @Override
     public boolean alcIsExtensionPresent(final String extension) {
         return ALC10.alcIsExtensionPresent(device, extension);
     }
 
+    @Override
     public void alcGetInteger(final int param, final IntBuffer buffer, final int size) {
         if (buffer.position() != 0) {
             throw new AssertionError();
@@ -86,12 +99,13 @@ public class LwjglALC implements com.jme3.audio.openal.ALC {
         ALC10.alcGetIntegerv(device, param, buffer);
     }
 
+    @Override
     public void alcDevicePauseSOFT() {
         SOFTPauseDevice.alcDevicePauseSOFT(device);
     }
 
+    @Override
     public void alcDeviceResumeSOFT() {
         SOFTPauseDevice.alcDeviceResumeSOFT(device);
     }
-
 }
