68ea05eaa63b3f30e0d32ba0446f316c80115ed0
Stephen Gold <sgold@sonic.net>
2018-11-20 23:40:19 -0800

    test and fix for JME issue #954 (null bounding volume)

--- jme3-core/src/main/java/com/jme3/scene/Node.java
+++ jme3-core/src/main/java/com/jme3/scene/Node.java
@@ -31,15 +31,14 @@
  */
 package com.jme3.scene;
 
+import com.jme3.bounding.BoundingBox;
 import com.jme3.bounding.BoundingVolume;
 import com.jme3.collision.Collidable;
 import com.jme3.collision.CollisionResults;
 import com.jme3.export.JmeExporter;
 import com.jme3.export.JmeImporter;
-import com.jme3.export.Savable;
 import com.jme3.material.Material;
 import com.jme3.util.SafeArrayList;
-import com.jme3.util.TempVars;
 import com.jme3.util.clone.Cloner;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -169,6 +168,9 @@ public class Node extends Spatial {
                     resultBound = child.getWorldBound().clone(this.worldBound);
                 }
             }
+        }
+        if (resultBound == null) {
+            resultBound = new BoundingBox(getWorldTranslation(), 0f, 0f, 0f);
         }
         this.worldBound = resultBound;
     }
