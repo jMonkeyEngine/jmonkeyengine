058d729e55030657a323ff6774967c1361d9cc71
Stephen Gold <sgold@sonic.net>
2019-02-26 13:13:12 -0800

    jme3-jbullet JavaDoc corrections (comments only)

--- jme3-jbullet/src/main/java/com/jme3/bullet/objects/PhysicsVehicle.java
+++ jme3-jbullet/src/main/java/com/jme3/bullet/objects/PhysicsVehicle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2018 jMonkeyEngine
+ * Copyright (c) 2009-2019 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -443,7 +443,7 @@ public class PhysicsVehicle extends PhysicsRigidBody {
 
     /**
      * Get the current speed of the vehicle in km/h
-     * @return
+     * @return the speed
      */
     public float getCurrentVehicleSpeedKmHour() {
         return vehicle.getCurrentSpeedKmHour();
