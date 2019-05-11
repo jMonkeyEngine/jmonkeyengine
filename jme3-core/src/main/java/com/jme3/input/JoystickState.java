76fcc2c497e61829c416e55bc9738f4cd71265c0
James Khan <jameskhan1983@gmail.com>
2019-05-11 20:18:56 +0100

    Remove magic number translations and remove uppercase names.

--- jme3-core/src/main/java/com/jme3/input/JoystickState.java
+++ jme3-core/src/main/java/com/jme3/input/JoystickState.java
@@ -6,42 +6,7 @@ package com.jme3.input;
  */
 public enum JoystickState {
 
-    // a list of connected/disconnected codes from various contexts.
-
-    // using the JoystickState.fromCode(int) method, if the code matches
-    // it will return the enum value.
-
-    CONNECTED(new int[] {
-            0x40001 // GLFW.GLFW_CONNECTED / LWJGL3
-    }),
-
-    DISCONNECTED(new int[] {
-            0x40002 // GLFW.GLFW_DISCONNECTED / LWJGL3
-    }),
-
-    UNKNOWN(new int[0]);
-
-    private int[] codes;
-
-    JoystickState(int[] codes) {
-        this.codes = codes;
-    }
-
-    private int[] getCodes() {
-        return codes;
-    }
-
-    public static JoystickState fromCode(int value) {
-
-        for (JoystickState state : values()) {
-            for (int code : state.getCodes()) {
-                if (value == code) {
-                    return state;
-                }
-            }
-        }
-
-        return UNKNOWN;
-    }
+    Connected,
+    Disconnected,
 
 }
