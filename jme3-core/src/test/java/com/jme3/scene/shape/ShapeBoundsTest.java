d8055f683863b3892a120b8bed0862dfe4818d5a
Lou Hamersly <louhamersly@gmail.com>
2019-06-16 19:50:24 -0400

    World bounds test enhancements for #1121

--- jme3-core/src/test/java/com/jme3/scene/shape/ShapeBoundsTest.java
+++ jme3-core/src/test/java/com/jme3/scene/shape/ShapeBoundsTest.java
@@ -36,6 +36,7 @@ import com.jme3.bounding.BoundingSphere;
 import com.jme3.bounding.BoundingVolume;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.VertexBuffer;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -51,8 +52,7 @@ public class ShapeBoundsTest {
     public void testBox() {
         Box shape = new Box(2f, 3f, 0.8f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
@@ -64,40 +64,35 @@ public class ShapeBoundsTest {
         controlp[3] = new Vector3f(3, 2, 1);
         Curve shape = new Curve(controlp, 32);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void testCylinder() {
         Cylinder shape = new Cylinder(16, 16, 2f, 3f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void testDome() {
         Dome shape = new Dome(16, 16, 5f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void testLine() {
         Line shape = new Line(new Vector3f(0, 0, 0), new Vector3f(1, 2, 3));
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void testPqTorus() {
         PQTorus shape = new PQTorus(2f, 3f, 0.8f, 0.2f, 64, 16);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
@@ -106,40 +101,56 @@ public class ShapeBoundsTest {
         Geometry geometry = new Geometry("geom", shape);
         BoundingVolume bv = geometry.getWorldBound();
         BoundingBox bb = (BoundingBox) bv;
-        Assert.assertTrue(bb.getXExtent() > 0 && bb.getYExtent() > 0);//Quad z extent 0 is normal
+        //Quad z extent 0 is normal, so not using testBounds() here.
+        Assert.assertTrue(bb.getXExtent() > 0 && bb.getYExtent() > 0);
+        testVertices(geometry);
     }
 
     @Test
     public void Sphere() {
         Sphere shape = new Sphere(32, 32, 5f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void StripBox() {
         StripBox shape = new StripBox(0.2f, 64f, 16f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
     @Test
     public void Torus() {
         Torus shape = new Torus(32, 32, 2f, 3f);
         Geometry geometry = new Geometry("geom", shape);
-        BoundingVolume bv = geometry.getWorldBound();
-        testBounds(bv);
+        testBounds(geometry);
     }
 
-    private void testBounds(BoundingVolume bv) {
+    private void testBounds(Geometry geometry) {
+        BoundingVolume bv = geometry.getWorldBound();
+
         if (bv instanceof BoundingBox) {
             BoundingBox bb = (BoundingBox) bv;
             Assert.assertTrue(bb.getXExtent() > 0 && bb.getYExtent() > 0 && bb.getZExtent() > 0);
         } else if (bv instanceof BoundingSphere) {
             BoundingSphere bs = (BoundingSphere) bv;
             Assert.assertTrue(bs.getRadius() > 1f);
+        }
+
+        testVertices(geometry);
+    }
+
+    private void testVertices(Geometry geometry) {
+        BoundingVolume bv = geometry.getWorldBound();
+        Assert.assertNotNull(bv);
+
+        for (int e = 0; e < geometry.getVertexCount(); e++) {
+            float x = (Float) geometry.getMesh().getBuffer(VertexBuffer.Type.Position).getElementComponent(e, 0);
+            float y = (Float) geometry.getMesh().getBuffer(VertexBuffer.Type.Position).getElementComponent(e, 1);
+            float z = (Float) geometry.getMesh().getBuffer(VertexBuffer.Type.Position).getElementComponent(e, 2);
+            Vector3f vertex = new Vector3f(x, y, z);
+            Assert.assertTrue("Vertex outside world bound: " + vertex, bv.intersects(vertex));
         }
     }
 }
