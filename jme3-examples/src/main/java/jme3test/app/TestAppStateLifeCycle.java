36afe829c63e70add1d350a6d1d7c73f80b4928e
Paul Speed <pspeed42@gmail.com>
2019-09-15 20:27:46 -0400

    Part 2 of the small AppState refactoring: added methods to AppStateManager for retrieving an AppState by ID.  Also modified attach() to throw an IllegalArgumentException if the ID is already registered. Updated TestAppStateLifeCycle to add a small ID demonstration.

--- jme3-examples/src/main/java/jme3test/app/TestAppStateLifeCycle.java
+++ jme3-examples/src/main/java/jme3test/app/TestAppStateLifeCycle.java
@@ -64,7 +64,10 @@ public class TestAppStateLifeCycle extends SimpleApplication {
         rootNode.attachChild(geom);
 
         System.out.println("Attaching test state.");
-        stateManager.attach(new TestState());        
+        stateManager.attach(new TestState());
+        
+        System.out.println("Attaching test state with an ID.");
+        stateManager.attach(new TestState("Test ID"));        
     }
 
     @Override
@@ -74,51 +77,64 @@ public class TestAppStateLifeCycle extends SimpleApplication {
             System.out.println("Detaching test state."); 
             stateManager.detach(stateManager.getState(TestState.class));
             System.out.println("Done"); 
-        }        
+        }
+        
+        if( stateManager.hasState("Test ID") ) {
+            System.out.println("Detaching test state with an ID."); 
+            stateManager.detach(stateManager.getState("Test ID", TestState.class));
+            System.out.println("Done"); 
+        }
     }
     
     public class TestState extends AbstractAppState {
  
+        public TestState() {
+        }
+        
+        public TestState( String id ) {
+            super(id);
+        }
+ 
         @Override
         public void initialize(AppStateManager stateManager, Application app) {
             super.initialize(stateManager, app);
-            System.out.println("Initialized");
+            System.out.println("Initialized, id:" + getId());
         }
  
         @Override
         public void stateAttached(AppStateManager stateManager) {
             super.stateAttached(stateManager);
-            System.out.println("Attached");
+            System.out.println("Attached, id:" + getId());
         }
  
         @Override
         public void update(float tpf) {
             super.update(tpf);
-            System.out.println("update");
+            System.out.println("update, id:" + getId());
         }
 
         @Override
         public void render(RenderManager rm) {
             super.render(rm);
-            System.out.println("render");
+            System.out.println("render, id:" + getId());
         }
 
         @Override
         public void postRender() {
             super.postRender();
-            System.out.println("postRender");
+            System.out.println("postRender, id:" + getId());
         }
 
         @Override
         public void stateDetached(AppStateManager stateManager) {
             super.stateDetached(stateManager);
-            System.out.println("Detached");
+            System.out.println("Detached, id:" + getId());
         }
  
         @Override
         public void cleanup() {
             super.cleanup();
-            System.out.println("Cleanup"); 
+            System.out.println("Cleanup, id:" + getId()); 
         }
 
     }    
