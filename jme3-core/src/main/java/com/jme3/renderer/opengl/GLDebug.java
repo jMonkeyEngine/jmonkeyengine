9be8f5dd3ab04fd85ddcb4a500f52c3c639ab966
Stephen Gold <sgold@sonic.net>
2020-02-02 15:52:56 +0100

    Use a reflection-based approach to call checkError() after every call to the openGL API to reduce Code Duplication and increase Maintainability, while also fixing the regression caused by GLDebugDesktop extending from GLDebugES and thus making the Renderer think it is on mobile.

--- jme3-core/src/main/java/com/jme3/renderer/opengl/GLDebug.java
+++ jme3-core/src/main/java/com/jme3/renderer/opengl/GLDebug.java
@@ -1,11 +1,62 @@
+/*
+ * Copyright (c) 2009-2020 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.jme3.renderer.opengl;
 
 import com.jme3.renderer.RendererException;
 
-public abstract class GLDebug {
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
+/**
+ * This class uses Reflection to intercept method calls to the Proxy Object ({@link #createProxy(GL, Object, Class[])}
+ * and extends them with the Error Checking in {@link #checkError()}.<br/>
+ * This means we don't have to generate a class with overrides for every possible method just to add a
+ * {@link #checkError()} call.<br/>
+ * Note that we should not call {@link #checkError()} for {@link GL#glGetError()}, it doesn't make sense.<br />
+ * Note that this class is general purpose and as such every class instance (every object) can be guarded as long as
+ * the passed gl instance is valid.<br/>
+ *
+ * @author MeFisto94
+ */
+public class GLDebug implements InvocationHandler {
+    protected Object obj;
     protected GL gl;
-    
+
+    private GLDebug(GL gl, Object obj) {
+        this.gl = gl;
+        this.obj = obj;
+    }
+
     protected String decodeError(int err) {
         String errMsg;
         switch (err) {
@@ -45,5 +96,34 @@ public abstract class GLDebug {
         if (err != 0) {
             throw new RendererException("An OpenGL error occurred - " + decodeError(err));
         }
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = method.invoke(obj, args);
+
+        if (method.getName().equals("glGetError")) {
+            return result;
+        }
+
+        checkError();
+        return result;
+    }
+
+    /**
+     * Creates a debug-proxied object, which will call {@link GL#glGetError()} after every method invocation and throw
+     * a {@link com.jme3.renderer.RendererException} if there was a GL Error.
+     *
+     * @param gl The GL Context, required to call {@link GL#glGetError()}
+     * @param obj The object which methods will be proxied
+     * @param implementedInterfaces The interfaces/class this object implements
+     * @return The Proxy object
+     */
+    public static Object createProxy(GL gl, Object obj, Class<?>... implementedInterfaces) {
+        return Proxy.newProxyInstance(
+            GLDebug.class.getClassLoader(),
+            implementedInterfaces,
+            new GLDebug(gl, obj)
+        );
     }
 }
