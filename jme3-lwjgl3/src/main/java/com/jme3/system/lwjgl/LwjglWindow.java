b93ea18fa2a238f250d882187cb123abeb5ba768
Toni Helenius <helenius.toni@gmail.com>
2020-02-26 03:20:09 +0200

    Lwjgl3 restart input handle (#1268)
    * Reinit inputs on context restart
    
    * Added test issue from issue #1013
    
    * Verify that the inputs are already initialized

--- jme3-lwjgl3/src/main/java/com/jme3/system/lwjgl/LwjglWindow.java
+++ jme3-lwjgl3/src/main/java/com/jme3/system/lwjgl/LwjglWindow.java
@@ -302,6 +302,11 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
         showWindow();
 
         allowSwapBuffers = settings.isSwapBuffers();
+
+        // Create OpenCL
+        if (settings.isOpenCLSupport()) {
+            initOpenCL(window);
+        }
     }
 
     protected void showWindow() {
@@ -310,6 +315,8 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
 
     /**
      * Set custom icons to the window of this application.
+     *
+     * @param settings settings for getting the icons
      */
     protected void setWindowIcon(final AppSettings settings) {
 
@@ -416,7 +423,6 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
                 window = NULL;
             }
 
-            glfwTerminate();
         } catch (final Exception ex) {
             listener.handleError("Failed to destroy context", ex);
         }
@@ -436,6 +442,8 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
 
     /**
      * Does LWJGL display initialization in the OpenGL thread
+     *
+     * @return returns {@code true} if the context initialization was successful
      */
     protected boolean initInThread() {
         try {
@@ -462,13 +470,6 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
 
             created.set(true);
             super.internalCreate();
-
-            //create OpenCL
-            //Must be done here because the window handle is needed
-            if (settings.isOpenCLSupport()) {
-                initOpenCL(window);
-            }
-
         } catch (Exception ex) {
             try {
                 if (window != NULL) {
@@ -493,14 +494,7 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
     protected void runLoop() {
         // If a restart is required, lets recreate the context.
         if (needRestart.getAndSet(false)) {
-            try {
-                destroyContext();
-                createContext(settings);
-            } catch (Exception ex) {
-                LOGGER.log(Level.SEVERE, "Failed to set display settings!", ex);
-            }
-
-            LOGGER.fine("Display restarted.");
+            restartContext();
         }
 
         if (!created.get()) {
@@ -556,6 +550,25 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
         glfwPollEvents();
     }
 
+    private void restartContext() {
+        try {
+            destroyContext();
+            createContext(settings);
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Failed to set display settings!", ex);
+        }
+
+        // We need to reinit the mouse and keyboard input as they are tied to a window handle
+        if (keyInput != null && keyInput.isInitialized()) {
+            keyInput.resetContext();
+        }
+        if (mouseInput != null && mouseInput.isInitialized()) {
+            mouseInput.resetContext();
+        }
+
+        LOGGER.fine("Display restarted.");
+    }
+
     private void setFrameRateLimit(int frameRateLimit) {
         this.frameRateLimit = frameRateLimit;
         frameSleepTime = 1000.0 / this.frameRateLimit;
@@ -569,6 +582,7 @@ public abstract class LwjglWindow extends LwjglContext implements Runnable {
 
         destroyContext();
         super.internalDestroy();
+        glfwTerminate();
 
         LOGGER.fine("Display destroyed.");
     }
