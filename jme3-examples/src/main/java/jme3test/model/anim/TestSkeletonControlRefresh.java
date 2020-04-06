56943046b3eeeb2805793b026baf89ac6e83d698
Jérôme <jerome@vollerin.fr>
2020-04-06 22:00:38 +0200

    Migrate TestSkeletonControlRefresh to new anim sys
    Migrate TestSkeletonControlRefresh to new Anim system

--- jme3-examples/src/main/java/jme3test/model/anim/TestSkeletonControlRefresh.java
+++ jme3-examples/src/main/java/jme3test/model/anim/TestSkeletonControlRefresh.java
@@ -36,8 +36,14 @@ package jme3test.model.anim;
  * @author Nehon
  */
 
+import java.util.ArrayList;
+import java.util.List;
 
-import com.jme3.animation.*;
+import com.jme3.anim.AnimComposer;
+import com.jme3.anim.SkinningControl;
+import com.jme3.anim.tween.Tween;
+import com.jme3.anim.tween.action.Action;
+import com.jme3.anim.tween.action.BaseAction;
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.TextureKey;
 import com.jme3.font.BitmapText;
@@ -46,34 +52,33 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
-import com.jme3.math.*;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.ssao.SSAOFilter;
-import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Quad;
 import com.jme3.shadow.DirectionalLightShadowFilter;
 
-import java.util.ArrayList;
-import java.util.List;
+public class TestSkeletonControlRefresh extends SimpleApplication implements ActionListener {
 
-//TODO rework this Test when the new animation system is done.
-public class TestSkeletonControlRefresh extends SimpleApplication implements ActionListener{
- 
-    private AnimChannel channel;
-    private AnimControl control;
-    private String[] animNames = {"Dodge", "Walk", "pull", "push"};
+    private AnimComposer animComposer;
+    private List<SkinningControl> skinningControls = new ArrayList<>();
+
     private final static int SIZE = 10;
-    private boolean hwSkinningEnable = true;
-    private List<SkeletonControl> skControls = new ArrayList<SkeletonControl>();
+    private boolean hwSkinningEnable = false;
     private BitmapText hwsText;
- 
+
     public static void main(String[] args) {
         TestSkeletonControlRefresh app = new TestSkeletonControlRefresh();
         app.start();
     }
- 
+
     @Override
     public void simpleInitApp() {
         viewPort.setBackgroundColor(ColorRGBA.White);
@@ -81,91 +86,109 @@ public class TestSkeletonControlRefresh extends SimpleApplication implements Act
         cam.setLocation(new Vector3f(3.8664846f, 6.2704787f, 9.664585f));
         cam.setRotation(new Quaternion(-0.054774776f, 0.94064945f, -0.27974048f, -0.18418397f));
         makeHudText();
- 
+
         DirectionalLight dl = new DirectionalLight();
         dl.setDirection(new Vector3f(-0.1f, -0.7f, -1).normalizeLocal());
         dl.setColor(new ColorRGBA(1f, 1f, 1f, 1.0f));
         rootNode.addLight(dl);
         Material m = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         TextureKey k = new TextureKey("Models/Oto/Oto.jpg", false);
-        m.setTexture("ColorMap", assetManager.loadTexture(k));        
- 
+        m.setTexture("ColorMap", assetManager.loadTexture(k));
+
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                Spatial model = assetManager.loadModel("Models/Oto/OtoOldAnim.j3o");
-                //setting a different material
+                Spatial model = assetManager.loadModel("Models/Oto/Oto.mesh.xml");
+                // setting a different material
                 model.setMaterial(m.clone());
                 model.setLocalScale(0.1f);
                 model.setLocalTranslation(i - SIZE / 2, 0, j - SIZE / 2);
-                control = model.getControl(AnimControl.class);
- 
-                channel = control.createChannel();
-                channel.setAnim(animNames[(i + j) % 4]);
-                channel.setLoopMode(LoopMode.DontLoop);
-                SkeletonControl skeletonControl = model.getControl(SkeletonControl.class);
-               
-                //This is a workaround the issue. this call will make the SkeletonControl gather the targets again.
-                //skeletonControl.setSpatial(model);
-                skeletonControl.setHardwareSkinningPreferred(hwSkinningEnable);
-                skControls.add(skeletonControl);
+
+                animComposer = model.getControl(AnimComposer.class);
+                animComposer.getAnimClips().forEach(animClip -> {
+                    Action action = animComposer.action(animClip.getName());
+                    animComposer.addAction(animClip.getName(), new NoLoopAction(action, animComposer));
+                });
+                animComposer.setCurrentAction(new ArrayList<>(animComposer.getAnimClips()).get((i + j) % 4).getName());
+
+                SkinningControl skinningControl = model.getControl(SkinningControl.class);
+                skinningControl.setHardwareSkinningPreferred(hwSkinningEnable);
+                skinningControls.add(skinningControl);
+
                 rootNode.attachChild(model);
             }
-        }        
-        
-        rootNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        }
+
+        rootNode.setShadowMode(ShadowMode.CastAndReceive);
         setupFloor();
-        
+
         inputManager.addListener(this, "toggleHWS");
         inputManager.addMapping("toggleHWS", new KeyTrigger(KeyInput.KEY_SPACE));
-        
-//        DirectionalLightShadowRenderer pssm = new DirectionalLightShadowRenderer(assetManager, 1024, 2);
-//        pssm.setLight(dl);
-//        viewPort.addProcessor(pssm);
-        
+
+        // DirectionalLightShadowRenderer pssm = new DirectionalLightShadowRenderer(assetManager, 1024, 2);
+        // pssm.setLight(dl);
+        // viewPort.addProcessor(pssm);
+
         FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
-        
+
         DirectionalLightShadowFilter sf = new DirectionalLightShadowFilter(assetManager, 1024, 2);
         sf.setLight(dl);
         fpp.addFilter(sf);
         fpp.addFilter(new SSAOFilter());
         viewPort.addProcessor(fpp);
-     
-        
+
     }
-    
-     public void setupFloor() {
+
+    public void setupFloor() {
         Quad q = new Quad(20, 20);
-       q.scaleTextureCoordinates(Vector2f.UNIT_XY.mult(10));
-       Geometry geom = new Geometry("floor", q);
-       Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-       mat.setColor("Color", ColorRGBA.White);       
-       geom.setMaterial(mat);
+        q.scaleTextureCoordinates(Vector2f.UNIT_XY.mult(10));
+        Geometry geom = new Geometry("floor", q);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.White);
+        geom.setMaterial(mat);
 
-       geom.rotate(-FastMath.HALF_PI, 0, 0);
-       geom.center();
-       geom.move(0, -0.3f, 0);
-       geom.setShadowMode(RenderQueue.ShadowMode.Receive);
-       rootNode.attachChild(geom);
+        geom.rotate(-FastMath.HALF_PI, 0, 0);
+        geom.center();
+        geom.move(0, -0.3f, 0);
+        geom.setShadowMode(ShadowMode.Receive);
+        rootNode.attachChild(geom);
     }
 
- 
     @Override
     public void onAction(String name, boolean isPressed, float tpf) {
-        if(isPressed && name.equals("toggleHWS")){
+        if (isPressed && name.equals("toggleHWS")) {
             hwSkinningEnable = !hwSkinningEnable;
-            for (SkeletonControl skControl : skControls) {
-                skControl.setHardwareSkinningPreferred(hwSkinningEnable);              
-                hwsText.setText("HWS : "+ hwSkinningEnable);
-            }
+            skinningControls.forEach(sc -> sc.setHardwareSkinningPreferred(hwSkinningEnable));
+            hwsText.setText("HWS : " + hwSkinningEnable);
         }
     }
- 
+
     private void makeHudText() {
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
         hwsText = new BitmapText(guiFont, false);
         hwsText.setSize(guiFont.getCharSet().getRenderedSize());
-        hwsText.setText("HWS : "+ hwSkinningEnable);
+        hwsText.setText("HWS : " + hwSkinningEnable);
         hwsText.setLocalTranslation(0, cam.getHeight(), 0);
         guiNode.attachChild(hwsText);
+    }
+
+    static class NoLoopAction extends BaseAction {
+
+        final AnimComposer ac;
+
+        public NoLoopAction(Tween delegate, AnimComposer ac) {
+            super(delegate);
+            this.ac = ac;
+        }
+
+        @Override
+        public boolean interpolate(double t) {
+            boolean running = super.interpolate(t);
+            if (!running) {
+                setSpeed(0);
+                ac.removeCurrentAction(AnimComposer.DEFAULT_LAYER);
+            }
+            return running;
+        }
+
     }
 }
