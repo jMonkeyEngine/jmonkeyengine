4361b5fb7efb89d278bccae21b560d58baf9221b
ItsMike54 <61209013+ItsMike54@users.noreply.github.com>
2020-04-08 17:47:40 -0400

    Update AppSettings.java

--- jme3-core/src/main/java/com/jme3/system/AppSettings.java
+++ jme3-core/src/main/java/com/jme3/system/AppSettings.java
@@ -237,33 +237,6 @@ public final class AppSettings extends HashMap<String, Object> {
      * @see AppSettings#setAudioRenderer(java.lang.String)
      */
     public static final String ANDROID_OPENAL_SOFT = "OpenAL_SOFT";
-    
-    /**
-     * Use JogAmp's JOGL as the display system, with the OpenGL forward compatible profile
-     * <p>
-     * N.B: This backend is EXPERIMENTAL
-     *
-     * @see AppSettings#setRenderer(java.lang.String)
-     */
-    public static final String JOGL_OPENGL_FORWARD_COMPATIBLE = "JOGL_OPENGL_FORWARD_COMPATIBLE";
-    
-    /**
-     * Use JogAmp's JOGL as the display system with the backward compatible profile
-     * <p>
-     * N.B: This backend is EXPERIMENTAL
-     *
-     * @see AppSettings#setRenderer(java.lang.String)
-     */
-    public static final String JOGL_OPENGL_BACKWARD_COMPATIBLE = "JOGL_OPENGL_BACKWARD_COMPATIBLE";
-    
-    /**
-     * Use JogAmp's JOAL as the display system
-     * <p>
-     * N.B: This backend is EXPERIMENTAL
-     *
-     * @see AppSettings#setRenderer(java.lang.String)
-     */
-    public static final String JOAL = "JOAL";
 
     static {
         defaults.put("Width", 640);
