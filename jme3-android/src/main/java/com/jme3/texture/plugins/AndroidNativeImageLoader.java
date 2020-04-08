0bdb0159be5ff6fe640f884700dbd1b8a12ff2bc
Ali-RS <ali_codmw@yahoo.com>
2020-04-08 13:14:04 +0430

    Fixed an issue with AndroidNativeImageLoader which was opening stream twice. (#1338)

--- jme3-android/src/main/java/com/jme3/texture/plugins/AndroidNativeImageLoader.java
+++ jme3-android/src/main/java/com/jme3/texture/plugins/AndroidNativeImageLoader.java
@@ -30,7 +30,7 @@ public class AndroidNativeImageLoader  implements AssetLoader {
         InputStream in = null;
         try {
             in = info.openStream();
-            return load(info.openStream(), flip, tmpArray);
+            return load(in, flip, tmpArray);
         } finally {
             if (in != null){
                 in.close();
