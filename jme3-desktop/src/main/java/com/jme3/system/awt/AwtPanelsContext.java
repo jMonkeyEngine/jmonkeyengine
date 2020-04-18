84912d5803431063083be125944597966d658e8e
Stephen Gold <sgold@sonic.net>
2020-04-18 11:35:51 -0700

    Fixes issue #1336: AWTPanels forcing OpenGL 2.0 (Compat Profile)

--- jme3-desktop/src/main/java/com/jme3/system/awt/AwtPanelsContext.java
+++ jme3-desktop/src/main/java/com/jme3/system/awt/AwtPanelsContext.java
@@ -210,7 +210,6 @@ public class AwtPanelsContext implements JmeContext {
 
     public void setSettings(AppSettings settings) {
         this.settings.copyFrom(settings);
-        this.settings.setRenderer(AppSettings.LWJGL_OPENGL2);
         if (actualContext != null){
             actualContext.setSettings(settings);
         }
