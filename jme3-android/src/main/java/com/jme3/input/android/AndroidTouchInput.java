7ae820f71fd20694dfee01e17f5083961ee621a7
rickard <rickard@mindemia.com>
2020-08-26 14:41:50 +0200

    ...

--- jme3-android/src/main/java/com/jme3/input/android/AndroidTouchInput.java
+++ jme3-android/src/main/java/com/jme3/input/android/AndroidTouchInput.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2012 jMonkeyEngine
+ * Copyright (c) 2009-2020 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -62,6 +62,7 @@ import java.util.logging.Logger;
  *
  * @author iwgeric
  */
+
 public class AndroidTouchInput implements TouchInput {
     private static final Logger logger = Logger.getLogger(AndroidTouchInput.class.getName());
 
