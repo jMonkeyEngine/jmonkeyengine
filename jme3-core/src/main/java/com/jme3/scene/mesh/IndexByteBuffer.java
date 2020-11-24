92a02833068590756cc9c92c11aa3b8912fe7d13
Stephen Gold <sgold@sonic.net>
2020-11-24 09:31:10 -0800

    add range-check assertions to the IndexBuffer.put() methods (#1427)

--- jme3-core/src/main/java/com/jme3/scene/mesh/IndexByteBuffer.java
+++ jme3-core/src/main/java/com/jme3/scene/mesh/IndexByteBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2012 jMonkeyEngine
+ * Copyright (c) 2009-2020 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -43,9 +43,34 @@ import com.jme3.scene.VertexBuffer.Format;
  */
 public class IndexByteBuffer extends IndexBuffer {
 
-    private ByteBuffer buf;
+    final private ByteBuffer buf;
+    /**
+     * the largest index value that can be put to the buffer
+     */
+    private int maxValue = 255;
 
+    /**
+     * Instantiate an IndexBuffer using the specified ByteBuffer and a maximum
+     * index value of 255.
+     *
+     * @param buffer a pre-existing buffer (not null, alias created)
+     */
     public IndexByteBuffer(ByteBuffer buffer) {
+        buf = buffer;
+        buf.rewind();
+    }
+
+    /**
+     * Instantiate an IndexBuffer using the specified ByteBuffer and set its
+     * maximum index value.
+     *
+     * @param buffer a pre-existing buffer (not null, alias created)
+     * @param maxValue the desired maximum index value (&ge;0, &le;255)
+     */
+    public IndexByteBuffer(ByteBuffer buffer, int maxValue) {
+        assert maxValue >= 0 && maxValue <= 255 : "out of range: " + maxValue;
+        this.maxValue = maxValue;
+
         buf = buffer;
         buf.rewind();
     }
@@ -62,12 +87,20 @@ public class IndexByteBuffer extends IndexBuffer {
 
     @Override
     public IndexByteBuffer put(int i, int value) {
+        assert value >= 0 && value <= maxValue 
+                : "IndexBuffer was created with elements too small for value="
+                + value;
+
         buf.put(i, (byte) value);
         return this;
     }
     
     @Override
     public IndexByteBuffer put(int value) {
+        assert value >= 0 && value <= maxValue 
+                : "IndexBuffer was created with elements too small for value="
+                + value;
+
         buf.put((byte) value);
         return this;
     }
