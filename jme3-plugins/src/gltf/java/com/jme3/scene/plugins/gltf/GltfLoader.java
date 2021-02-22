4e9e1f0fd7321bd45b452dd2eb73af2cdff99988
Xenokieser <56299278+Xenokieser@users.noreply.github.com>
2021-02-22 19:12:16 +0100

    Update GltfLoader.java

--- jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/GltfLoader.java
+++ jme3-plugins/src/gltf/java/com/jme3/scene/plugins/gltf/GltfLoader.java
@@ -1087,8 +1087,7 @@ public class GltfLoader implements AssetLoader {
     private void findChildren(int nodeIndex) throws IOException {
         JointWrapper jw = fetchFromCache("nodes", nodeIndex, JointWrapper.class);
         
-        //Check if jw node exist or leave the function
-        if(jw == null){
+        if(jw == null) {
             return;
         }
         
