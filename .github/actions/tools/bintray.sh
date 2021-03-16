abe7f38cc8a560712b23f77ef4cd08a5c00ceef6
Stephen Gold <sgold@sonic.net>
2021-03-16 13:06:36 -0700

    remove scripting that's used only during deployment to Bintray

--- .github/actions/tools/bintray.sh
+++ .github/actions/tools/bintray.sh
@@ -1,40 +1,5 @@
 #!/bin/bash
 
-# bintray_createPackage [REPO] [PACKAGE] [USER] [PASSWORD] [GIT REPO] [LICENSE]
-function bintray_createPackage {
-    repo="$1"
-    package="$2"
-    user="$3"
-    password="$4"
-    srcrepo="$5"
-    license="$6"
-
-    repoUrl="https://api.bintray.com/packages/$repo"
-    if [ "`curl -u$user:$password -H Content-Type:application/json -H Accept:application/json \
-    --write-out %{http_code} --silent --output /dev/null -X GET \"$repoUrl/$package\"`" != "200" ];
-    then
-
-        if [ "$srcrepo" != "" -a "$license" != "" ];
-        then 
-            echo "Package does not exist... create."
-            data="{
-                \"name\": \"${package}\",
-                \"labels\": [],
-                \"licenses\": [\"${license}\"],
-                \"vcs_url\": \"${srcrepo}\"
-            }"
-     
-
-            curl -u$user:$password -H "Content-Type:application/json" -H "Accept:application/json" -X POST \
-                -d "${data}" "$repoUrl"
-        else
-            echo "Package does not exist... you need to specify a repo and license for it to be created."
-        fi
-    else    
-        echo "The package already exists. Skip."
-    fi
-}
-
 # minio_uploadFile <LOCAL_FILEPATH> <REMOTE_FILEPATH> <MINIO_URL> <MINIO_ACCESS_KEY> <MINIO_SECRET_KEY>
 #
 # Upload the specified file to the specified MinIO instance.
