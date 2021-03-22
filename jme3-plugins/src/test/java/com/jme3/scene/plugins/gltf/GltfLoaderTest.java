62dc29b9481532b436bff96b89723dd53ad55f45
Markil3 <75867393+Markil3@users.noreply.github.com>
2021-03-22 12:52:17 -0600

    Gltf unlit (#1463)
    * Adds support for the Unlit material in the GLTF loader.
    
    * Adds some author information.
    
    * Removes an unnecessary test.
    
    It was left over from development and inevitably fails. TestGLTFUnlit serves as a good enough test anyway.
    
    * Adds the copyright notice to TestGltfUnlit.

--- jme3-plugins/src/test/java/com/jme3/scene/plugins/gltf/GltfLoaderTest.java
+++ jme3-plugins/src/test/java/com/jme3/scene/plugins/gltf/GltfLoaderTest.java
@@ -34,9 +34,11 @@ package com.jme3.scene.plugins.gltf;
 import com.jme3.asset.AssetLoadException;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.plugin.TestMaterialWrite;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.system.JmeSystem;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -78,10 +80,15 @@ public class GltfLoaderTest {
     }
 
     private void dumpScene(Spatial s, int indent) {
-        System.err.println(indentString.substring(0, indent) + s.getName() + " (" + s.getClass().getSimpleName() + ") / " +
+        System.err.print(indentString.substring(0, indent) + s.getName() + " (" + s.getClass().getSimpleName() + ") / " +
                 s.getLocalTransform().getTranslation().toString() + ", " +
                 s.getLocalTransform().getRotation().toString() + ", " +
                 s.getLocalTransform().getScale().toString());
+        if (s instanceof Geometry)
+        {
+            System.err.print(" / " + ((Geometry) s).getMaterial());
+        }
+        System.err.println();
         if (s instanceof Node) {
             Node n = (Node) s;
             for (Spatial spatial : n.getChildren()) {
