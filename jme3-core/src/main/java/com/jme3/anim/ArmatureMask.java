3448a32349cacd89c7d3f96e451756f246b66c10
Stephen Gold <sgold@sonic.net>
2021-04-03 12:14:41 -0700

    resolve issue #1499 (more constructors and methods for ArmatureMask)

--- jme3-core/src/main/java/com/jme3/anim/ArmatureMask.java
+++ jme3-core/src/main/java/com/jme3/anim/ArmatureMask.java
@@ -6,6 +6,57 @@ public class ArmatureMask implements AnimationMask {
 
     final private BitSet affectedJoints = new BitSet();
 
+    /**
+     * Instantiate a mask that affects no joints.
+     */
+    public ArmatureMask() {
+        // do nothing
+    }
+
+    /**
+     * Instantiate a mask that affects all joints in the specified Armature.
+     *
+     * @param armature the Armature containing the joints (not null, unaffected)
+     */
+    public ArmatureMask(Armature armature) {
+        int numJoints = armature.getJointCount();
+        affectedJoints.set(0, numJoints);
+    }
+
+    /**
+     * Remove all joints affected by the specified ArmatureMask.
+     *
+     * @param removeMask the set of joints to remove (not null, unaffected)
+     * @return this
+     */
+    public ArmatureMask remove(ArmatureMask removeMask) {
+        BitSet removeBits = removeMask.getAffectedJoints();
+        affectedJoints.andNot(removeBits);
+
+        return this;
+    }
+
+    private BitSet getAffectedJoints() {
+        return affectedJoints;
+    }
+
+    /**
+     * Remove the named joints.
+     *
+     * @param armature the Armature containing the joints (not null, unaffected)
+     * @param jointNames the names of the joints to be removed
+     * @return this
+     */
+    public ArmatureMask removeJoints(Armature armature, String... jointNames) {
+        for (String jointName : jointNames) {
+            Joint joint = findJoint(armature, jointName);
+            int jointId = joint.getId();
+            affectedJoints.clear(jointId);
+        }
+
+        return this;
+    }
+
     @Override
     public boolean contains(Object target) {
         return affectedJoints.get(((Joint) target).getId());
@@ -59,4 +110,33 @@ public class ArmatureMask implements AnimationMask {
         }
     }
 
+    /**
+     * Add the specified Joint and all its ancestors.
+     *
+     * @param start the starting point (may be null, unaffected)
+     * @return this
+     */
+    public ArmatureMask addAncestors(Joint start) {
+        for (Joint cur = start; cur != null; cur = cur.getParent()) {
+            int jointId = cur.getId();
+            affectedJoints.set(jointId);
+        }
+
+        return this;
+    }
+
+    /**
+     * Remove the specified Joint and all its ancestors.
+     *
+     * @param start the starting point (may be null, unaffected)
+     * @return this
+     */
+    public ArmatureMask removeAncestors(Joint start) {
+        for (Joint cur = start; cur != null; cur = cur.getParent()) {
+            int jointId = cur.getId();
+            affectedJoints.clear(jointId);
+        }
+
+        return this;
+    }
 }
