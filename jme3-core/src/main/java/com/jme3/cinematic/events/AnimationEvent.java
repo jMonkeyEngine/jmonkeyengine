3f706b67d86257e5c66edb564fc56384c0d0e58d
Stephen Gold <sgold@sonic.net>
2021-04-04 21:28:34 -0700

    AnimationEvent: add deprecation advice to the javadoc

--- jme3-core/src/main/java/com/jme3/cinematic/events/AnimationEvent.java
+++ jme3-core/src/main/java/com/jme3/cinematic/events/AnimationEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2019 jMonkeyEngine
+ * Copyright (c) 2009-2021 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,6 +51,7 @@ import java.util.logging.Logger;
  *
  *
  * @author Nehon
+ * @deprecated use {@link AnimEvent}
  */
 @Deprecated
 public class AnimationEvent extends AbstractCinematicEvent {
