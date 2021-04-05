jMonkeyEngine/jmonkeyengine

commit 1031e03d7249e090a606cd8f2fb5f33f7645a400

add private constructors to utility classes

jme3-core/src/main/java/com/jme3/input/KeyNames.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2012 jMonkeyEngine
+ * Copyright (c) 2009-2021 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -178,6 +178,12 @@ public class KeyNames {
         KEY_NAMES[KEY_KANJI] = "Kanji";
         KEY_NAMES[KEY_AX] = "Ax";
         KEY_NAMES[KEY_UNLABELED] = "Unlabeled";
+    }
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private KeyNames() {
     }
 
     public static String getName(int keyId) {

commit 145cc1bbdd766326e6ca9bf1722fbdb6460f6697

KeyNames: partial revert of 1031e03d which broke Lemur

jme3-core/src/main/java/com/jme3/input/KeyNames.java
@@ -33,6 +33,15 @@ package com.jme3.input;
 
 import static com.jme3.input.KeyInput.*;
 
+/**
+ * Translate key codes (from {@link KeyInput}) to descriptive names.
+ *
+ * This class has no static methods, yet it can be instantiated. Here's why:
+ *
+ * It used to be that there was no static getName() method, so the only way to
+ * get names was to instantiate a KeyNames. As a consequence, we have
+ * applications and libraries that rely on being able to instantiate this class.
+ */
 public class KeyNames {
 
     private static final String[] KEY_NAMES = new String[0xFF];
@@ -178,12 +187,6 @@ public class KeyNames {
         KEY_NAMES[KEY_KANJI] = "Kanji";
         KEY_NAMES[KEY_AX] = "Ax";
         KEY_NAMES[KEY_UNLABELED] = "Unlabeled";
-    }
-
-    /**
-     * A private constructor to inhibit instantiation of this class.
-     */
-    private KeyNames() {
     }
 
     public static String getName(int keyId) {