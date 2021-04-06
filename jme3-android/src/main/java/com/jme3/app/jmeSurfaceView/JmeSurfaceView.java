jMonkeyEngine/jmonkeyengine

commit 3fbc83b52ee3f01db73b6790feddf21caf60e67b

Added JmeSurfaceView.java class , for embedding jme-game in a custom xml UI-design. (#1494)
* Added JmeSurfaceView.java class , for embedding jme-game in a custom xml UI design.

* Removal of white spaces

Removal of white spaces between if...else conditions & class methods.

* Removal of white spaces

* Cleaning up (formatting errors/jme copyright)

* Moved the class files into an independent package

* Handled the user delay for zero & negative numbers

* Cleaning up 2

jme3-android/src/main/java/com/jme3/app/jmeSurfaceView/OnExceptionThrown.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2009-2020 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.app.jmeSurfaceView;
+
+/**
+ * Embedded interface designed to listen to exceptions & fire when an exception is thrown.
+ * @see JmeSurfaceView#setOnExceptionThrown(OnExceptionThrown).
+ */
+public interface OnExceptionThrown {
+    /**
+     * Listens for a thrown exception or a thrown error.
+     * @param e the exception or the error that is throwable.
+     */
+    void onExceptionThrown(Throwable e);
+}

jme3-android/src/main/java/com/jme3/app/jmeSurfaceView/OnRendererCompleted.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (c) 2009-2020 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.app.jmeSurfaceView;
+
+import com.jme3.app.LegacyApplication;
+import com.jme3.system.AppSettings;
+
+/**
+ * Embedded interface class(abstract) to listen for the moment when when the GL thread holding the #{@link JmeSurfaceView}
+ * joins the UI thread , after asynchronous rendering.
+ * @see JmeSurfaceView#setOnRendererCompleted(OnRendererCompleted).
+ */
+public interface OnRendererCompleted {
+    /**
+     * Listens for the the moment when when the GL thread holding the #{@link JmeSurfaceView}
+     * joins the UI thread , after asynchronous rendering.
+     * @param application the current jme game instance.
+     * @param appSettings the current window settings of the running jme game.
+     */
+    void onRenderCompletion(LegacyApplication application, AppSettings appSettings);
+}

jme3-android/build.gradle
@@ -5,6 +5,8 @@ if (!hasProperty('mainClass')) {
 }
 
 dependencies {
+    //added annotations used by JmeSurfaceView.
+    implementation 'androidx.appcompat:appcompat:1.2.0'
     compile project(':jme3-core')
     compile project(':jme3-plugins')
     compileOnly 'android:android'

commit 60e1a21d3080f4d6a7a61afa8cbd49e310a3c822

jme3-android: resolve javadoc diagnostics caused by 3fbc83b5

jme3-android/src/main/java/com/jme3/app/jmeSurfaceView/JmeSurfaceView.java
@@ -56,7 +56,7 @@ import java.util.logging.Logger;
 /**
  * <b>A RelativeLayout Class Holder that holds a #{{@link GLSurfaceView}} using #{{@link OGLESContext}} as a renderer to render
  * a JME game on an android view for custom xmL designs.</b>
- * @apiNote The main idea of #{@link JmeSurfaceView} class is to start a jMonkeyEngine application in a SystemListener#{@link SystemListener} context in a GL_ES thread ,
+ * The main idea of #{@link JmeSurfaceView} class is to start a jMonkeyEngine application in a {@link SystemListener} context in a GL_ES thread ,
  * then the GLSurfaceView holding the GL_ES thread joins the UI thread with a delay of user's choice using a #{@link Handler} , during the delay , the game runs normally in the GL_ES thread(but without coming up on the UI)
  * and the user has the ability to handle a couple of actions asynchronously as displaying a progress bar on a SplashScreen or
  * an image or even play a preface game music of choice.
@@ -104,8 +104,8 @@ public class JmeSurfaceView extends RelativeLayout implements SystemListener, Di
     /**
      * starts the jmeRenderer on a GlSurfaceView attached to a RelativeLayout.
      * @param delayMillis delay of the appearance of jme game on the screen , this doesn't delay the renderer though.
-     * @apiNote use #{@link JmeSurfaceView#NO_DELAY} to disable the delay ,
-     * any values less than 1ms#{@link JmeSurfaceView#NO_DELAY} would be ignored & the delay would be disabled.
+     * Use #{@link JmeSurfaceView#NO_DELAY} to disable the delay ,
+     * any values less than 1ms#{@link JmeSurfaceView#NO_DELAY} would be ignored and the delay would be disabled.
      */
     public void startRenderer(int delayMillis) {
         this.delayMillis = Math.max(NO_DELAY, delayMillis);
@@ -271,7 +271,7 @@ public class JmeSurfaceView extends RelativeLayout implements SystemListener, Di
     }
 
     /**
-     * Displays an error dialog with a throwable message(error/exception) & with 2 buttons.
+     * Displays an error dialog with a throwable message(error/exception) and 2 buttons.
      * @param throwable the throwable stack.
      * @param message the string message.
      */
@@ -369,7 +369,7 @@ public class JmeSurfaceView extends RelativeLayout implements SystemListener, Di
      * the depth buffer or Z-buffer is basically coupled with stencil buffer ,
      * usually 8bits stencilBuffer + 24bits depthBuffer = 32bits shared memory.
      * @param eglDepthBits the depth bits.
-     * @see JmeSurfaceView#setEglStencilBits(int).
+     * @see JmeSurfaceView#setEglStencilBits(int)
      */
     public void setEglDepthBits(int eglDepthBits) {
         this.eglDepthBits = eglDepthBits;
@@ -403,11 +403,11 @@ public class JmeSurfaceView extends RelativeLayout implements SystemListener, Di
 
     /**
      * Set the number of stencil buffer bits.
-     * Stencil buffer is used in depth-based shadow maps & shadow rendering as it limits rendering,
+     * Stencil buffer is used in depth-based shadow maps and shadow rendering as it limits rendering,
      * it's coupled with Z-buffer or depth buffer,usually 8bits stencilBuffer + 24bits depthBuffer = 32bits shared memory.
      * (default = 0)
      * @param eglStencilBits the desired number of stencil bits.
-     * @see JmeSurfaceView#setEglDepthBits(int).
+     * @see JmeSurfaceView#setEglDepthBits(int)
      */
     public void setEglStencilBits(int eglStencilBits) {
         this.eglStencilBits = eglStencilBits;
@@ -431,7 +431,7 @@ public class JmeSurfaceView extends RelativeLayout implements SystemListener, Di
 
     /**
      * gets the limited FrameRate level for egl config.
-     * @implNote
+     *
      * @return the limit frameRate in integers.
      */
     public int getFrameRate() {

jme3-android/src/main/java/com/jme3/app/jmeSurfaceView/OnExceptionThrown.java
@@ -32,8 +32,8 @@
 package com.jme3.app.jmeSurfaceView;
 
 /**
- * Embedded interface designed to listen to exceptions & fire when an exception is thrown.
- * @see JmeSurfaceView#setOnExceptionThrown(OnExceptionThrown).
+ * Embedded interface designed to listen to exceptions and fire when an exception is thrown.
+ * @see JmeSurfaceView#setOnExceptionThrown(OnExceptionThrown)
  */
 public interface OnExceptionThrown {
     /**

jme3-android/src/main/java/com/jme3/app/jmeSurfaceView/OnRendererCompleted.java
@@ -37,7 +37,7 @@ import com.jme3.system.AppSettings;
 /**
  * Embedded interface class(abstract) to listen for the moment when when the GL thread holding the #{@link JmeSurfaceView}
  * joins the UI thread , after asynchronous rendering.
- * @see JmeSurfaceView#setOnRendererCompleted(OnRendererCompleted).
+ * @see JmeSurfaceView#setOnRendererCompleted(OnRendererCompleted)
  */
 public interface OnRendererCompleted {
     /**