0de0a3f0e8008a938e9ed82e7214bb7cb9dcd32a
Stephen Gold <sgold@sonic.net>
2021-04-06 12:42:20 -0700

    FlyByCamera: delete unused import

--- jme3-core/src/main/java/com/jme3/input/FlyByCamera.java
+++ jme3-core/src/main/java/com/jme3/input/FlyByCamera.java
@@ -37,7 +37,6 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
-import com.jme3.math.FastMath;
 import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
