39628fc92bcf704b4643086190285f0a07c53721
Scrappers <scrappers.tm@gmail.com>
2021-10-12 01:05:35 -0500

    Action & BaseAction classes java docs

--- jme3-core/src/main/java/com/jme3/anim/tween/action/BaseAction.java
+++ jme3-core/src/main/java/com/jme3/anim/tween/action/BaseAction.java
@@ -6,10 +6,20 @@ import com.jme3.util.SafeArrayList;
 
 import java.util.List;
 
+/**
+ * A Concrete fast Implementation for the interface #{@link Action},
+ * used to create a subset of actions gathered from the extracted actions of the assigned tween.
+ * <b>Inspired by Nehon.</b>
+ */
 public class BaseAction extends Action {
 
     final private Tween tween;
 
+    /**
+     * Gathers the actions from a tween into a subset collection #{@link Action#actions},
+     * which joins anim actions hierarchy later on when instantiated by #{@link BlendableAction} implementation entites.
+     * @param tween the tween to join the actions collection, either raw tween using #{@link com.jme3.anim.tween.Tweens} utility or #{@link Action} tween.
+     */
     public BaseAction(Tween tween) {
         this.tween = tween;
         setLength(tween.getLength());
@@ -19,10 +29,18 @@ public class BaseAction extends Action {
         subActions.toArray(actions);
     }
 
+    /**
+     * Used to add the tween actions only if they are types of anim #{@link Action} class.
+     * Otherwise if they are in a raw tween state #{@link Tween}.
+     * then fetch the children actions of this tween & add them to subset of actions collection.
+     * @param tween the tween to add, either raw tween using #{@link com.jme3.anim.tween.Tweens} utility or #{@link Action} tween.
+     * @param subActions the collection to gather the tween actions into #{@link Action#actions} collection.
+     */
     private void gatherActions(Tween tween, List<Action> subActions) {
         if (tween instanceof Action) {
             subActions.add((Action) tween);
         } else if (tween instanceof ContainsTweens) {
+            //extract the tween action from the tween
             Tween[] tweens = ((ContainsTweens) tween).getTweens();
             for (Tween t : tweens) {
                 gatherActions(t, subActions);
@@ -32,6 +50,8 @@ public class BaseAction extends Action {
 
     @Override
     public boolean interpolate(double t) {
+        /*interpolate -> calls doInterpolate(t:Double) -> which interpolate between
+        the concurrent transform matrices starting from the current transform matrix based on frame times.*/
         return tween.interpolate(t);
     }
 }
