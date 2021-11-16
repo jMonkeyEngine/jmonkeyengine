953cc0fd5d1e8aa6ef736153f8c1dd3333dc104a
Stephen Gold <sgold@sonic.net>
2021-11-16 09:49:04 -0800

    jme3-core/src/test:  upgrade MockitoJUnitRunner from deprecated version

--- jme3-core/src/test/java/com/jme3/material/MaterialTest.java
+++ jme3-core/src/test/java/com/jme3/material/MaterialTest.java
@@ -50,7 +50,7 @@ import java.util.EnumSet;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MaterialTest {
