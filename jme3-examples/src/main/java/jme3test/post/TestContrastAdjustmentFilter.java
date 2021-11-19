918feeecd0b6c6defc13b5a1bd491323eefeb887
Scrappers <scrappers.tm@gmail.com>
2021-11-19 16:57:04 -0600

    update the testcase to meet the new changes

--- jme3-examples/src/main/java/jme3test/post/TestContrastAdjustmentFilter.java
+++ jme3-examples/src/main/java/jme3test/post/TestContrastAdjustmentFilter.java
@@ -1,20 +1,57 @@
+/*
+ * Copyright (c) 2009-2020 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package jme3test.post;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.AmbientLight;
-import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.filters.ContrastAdjustmentFilter;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.texture.Texture;
 
-
+/**
+ * Tests {@link ContrastAdjustmentFilter} on different color channels with an
+ * adjustable scale and brightness during runtime.
+ * @author pavl_g.
+ */
 public class TestContrastAdjustmentFilter extends SimpleApplication {
+
+    private float counter = 0f;
+    private ContrastAdjustmentFilter contrastAdjustmentFilter;
 
     public static void main(String[] args) {
         new TestContrastAdjustmentFilter().start();
@@ -23,7 +60,7 @@ public class TestContrastAdjustmentFilter extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         //setup a spatial and a texture
-        final Quad quad = new Quad(40, 20);
+        final Sphere quad = new Sphere(40, 40, 5f);
         final Geometry quadGeo = new Geometry("Ball", quad);
         final Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         final Texture texture = assetManager.loadTexture("Textures/Sky/Earth/Earth.jpg");
@@ -38,8 +75,27 @@ public class TestContrastAdjustmentFilter extends SimpleApplication {
 
         //setup the filter
         final FilterPostProcessor postProcessor = new FilterPostProcessor(assetManager);
-        final ContrastAdjustmentFilter contrastAdjustmentFilter = new ContrastAdjustmentFilter(2.2f, 2.2f, 2.2f);
+        //setting up some parameters
+        final Vector3f exponents = new Vector3f(1.8f, 1.8f, 2.1f);
+        final Vector3f scales = new Vector3f(0.25f, 0.25f, 1f);
+        final Vector2f brightness = new Vector2f(0, 0.367f);
+        contrastAdjustmentFilter = new ContrastAdjustmentFilter(exponents, brightness, scales);
         postProcessor.addFilter(contrastAdjustmentFilter);
         viewPort.addProcessor(postProcessor);
+
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        counter += tpf;
+        if(counter >= 4f){
+            counter = 0f;
+        }
+        if(contrastAdjustmentFilter != null){
+            //adjust the transfer function during runtime
+            contrastAdjustmentFilter.setScales(counter * 0.5f, counter, counter * 0.5f);
+            contrastAdjustmentFilter.setExponents(counter, counter * 2f, counter);
+            contrastAdjustmentFilter.setBrightness(0, 1/counter);
+        }
     }
 }
