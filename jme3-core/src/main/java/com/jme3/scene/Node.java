662c8fe0c1117e7c65b77dd81c8816d4052c66c9
vxel <vxel@users.noreply.github.com>
2021-11-24 19:27:00 +0100

    fix issue 1672 (inefficient logging in Node.java) (#1673)
    * #1672 Logging statements should be guarded
    
    * #1672 Fix performance penalty of logger without guard
    
    Co-authored-by: Cédric de Launois <>

--- jme3-core/src/main/java/com/jme3/scene/Node.java
+++ jme3-core/src/main/java/com/jme3/scene/Node.java
@@ -431,7 +431,7 @@ public class Node extends Spatial {
         Spatial child = children.remove(index);
         if (child != null) {
             child.setParent(null);
-            logger.log(Level.FINE, "{0}: Child removed.", this.toString());
+            logger.log(Level.FINE, "{0}: Child removed.", this);
 
             // since a child with a bound was detached;
             // our own bound will probably change.
@@ -461,7 +461,7 @@ public class Node extends Spatial {
         for (int i = children.size() - 1; i >= 0; i--) {
             detachChildAt(i);
         }
-        logger.log(Level.FINE, "{0}: All children removed.", this.toString());
+        logger.log(Level.FINE, "{0}: All children removed.", this);
     }
 
     /**
