56543fcffefc24fb6302109f39b557d139b1eb02
Stephen Gold <sgold@sonic.net>
2021-12-14 10:59:16 -0800

    TestMultiRenderTarget:  additional documentation

--- jme3-examples/src/main/java/jme3test/post/TestMultiRenderTarget.java
+++ jme3-examples/src/main/java/jme3test/post/TestMultiRenderTarget.java
@@ -47,20 +47,54 @@ import com.jme3.texture.Image.Format;
 import com.jme3.texture.Texture2D;
 import com.jme3.ui.Picture;
 
+/**
+ * Demonstrates rendering to multiple texture targets of a single FrameBuffer.
+ *
+ * <p>The GUI viewport is tiled into 4 pictures,
+ * each displaying a different render of 5 colorful cubes in the main scene.
+ */
 public class TestMultiRenderTarget extends SimpleApplication implements SceneProcessor {
     private FrameBuffer fb;
+    /**
+     * Displays the merged RGB (normal color) output<ul>
+     * <li>from "ExtractRGB.frag" location 3,</li>
+     * <li>in the lower-left quadrant of the window.</li></ul>
+     */
     private Picture display1;
+    /**
+     * Displays the red-channel output in color<ul>
+     * <li>from "ExtractRGB.frag" location 0,</li>
+     * <li>in the upper-left quadrant of the window.</li></ul>
+     */
     private Picture display2;
+    /**
+     * Displays the green-channel output in monochrome<ul>
+     * <li>from ExtractRGB.frag location 1,</li>
+     * <li>in the upper-right quadrant of the window.</li></ul>
+     */
     private Picture display3;
+    /**
+     * Displays the blue-channel output in monochrome<ul>
+     * <li>from ExtractRGB.frag location 2,</li>
+     * <li>in the lower-right quadrant of the window.</li></ul>
+     */
     private Picture display4;
 
     private boolean initialized = false;
 
+    /**
+     * The main entry point for the TestMultiRenderTarget application.
+     *
+     * @param args unused
+     */
     public static void main(String[] args){
         TestMultiRenderTarget app = new TestMultiRenderTarget();
         app.start();
     }
 
+    /**
+     * Add 5 colorful cubes to the main scene.
+     */
     protected void buildScene() {
         Geometry cube1 = buildCube(ColorRGBA.Red);
         cube1.setLocalTranslation(-1f, 0f, 0f);
@@ -81,6 +115,17 @@ public class TestMultiRenderTarget extends SimpleApplication implements ScenePro
         rootNode.attachChild(cube5);
     }
 
+    /**
+     * Create a cube with the specified color,
+     * using a custom unshaded material that outputs 4 textures:<ul>
+     * <li>red channel only to location 0,</li>
+     * <li>green channel only to location 1,</li>
+     * <li>blue channel only to location 2,</li>
+     * <li>merged RGB to location 3.</li></ul>
+     *
+     * @param color the desired albedo color (alias created)
+     * @return a new Geometry with no parent
+     */
     private Geometry buildCube(ColorRGBA color) {
         Geometry cube = new Geometry("Box", new Box(0.5f, 0.5f, 0.5f));
         Material mat = new Material(assetManager, "TestMRT/MatDefs/ExtractRGB.j3md");
@@ -124,16 +169,16 @@ public class TestMultiRenderTarget extends SimpleApplication implements ScenePro
     @Override
     public void reshape(ViewPort vp, int w, int h) {
         // You can use multiple channel formats as well. That's why red is using RGBA8 as an example.
-        Texture2D redTexture = new Texture2D(w, h, Format.RGBA8);
-        Texture2D greenTexture = new Texture2D(w, h, Format.Luminance8);
-        Texture2D blueTexture = new Texture2D(w, h, Format.Luminance8);
-        Texture2D rgbTexture = new Texture2D(w, h, Format.RGBA8);
+        Texture2D redTexture = new Texture2D(w, h, Format.RGBA8);        // color texture
+        Texture2D greenTexture = new Texture2D(w, h, Format.Luminance8); // monochrome texture
+        Texture2D blueTexture = new Texture2D(w, h, Format.Luminance8);  // monochrome texture
+        Texture2D rgbTexture = new Texture2D(w, h, Format.RGBA8);        // color texture
 
         fb = new FrameBuffer(w, h, 1);
-        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(redTexture));
-        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(greenTexture));
-        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(blueTexture));
-        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(rgbTexture));
+        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(redTexture));   // location 0
+        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(greenTexture)); // location 1
+        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(blueTexture));  // location 2
+        fb.addColorTarget(FrameBuffer.FrameBufferTarget.newTarget(rgbTexture));   // location 3
         fb.setMultiTarget(true);
 
         display1.setTexture(assetManager, rgbTexture, false);
@@ -141,19 +186,19 @@ public class TestMultiRenderTarget extends SimpleApplication implements ScenePro
         display3.setTexture(assetManager, greenTexture, false);
         display4.setTexture(assetManager, blueTexture, false);
 
-        display1.setPosition(0, 0);
+        display1.setPosition(0, 0); // lower-left quadrant
         display1.setWidth(w/2f);
         display1.setHeight(h/2f);
 
-        display2.setPosition(0, h/2f);
+        display2.setPosition(0, h/2f); // upper-left quadrant
         display2.setWidth(w/2f);
         display2.setHeight(h/2f);
 
-        display3.setPosition(w/2f, h/2f);
+        display3.setPosition(w/2f, h/2f); // upper-right quadrant
         display3.setWidth(w/2f);
         display3.setHeight(h/2f);
 
-        display4.setPosition(w/2f, 0f);
+        display4.setPosition(w/2f, 0f); // lower-right quadrant
         display4.setWidth(w/2f);
         display4.setHeight(h/2f);
 
