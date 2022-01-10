dae8df7f9c31a993f5a021490af015f3ee3a75d9
Stephen Gold <sgold@sonic.net>
2022-01-10 11:59:07 -0800

    AssetLoadException:  add vertical whitespace required by 4.6.1

--- jme3-core/src/main/java/com/jme3/asset/AssetLoadException.java
+++ jme3-core/src/main/java/com/jme3/asset/AssetLoadException.java
@@ -42,6 +42,7 @@ public class AssetLoadException extends RuntimeException {
     public AssetLoadException(String message) {
         super(message);
     }
+
     public AssetLoadException(String message, Throwable cause) {
         super(message, cause);
     }
