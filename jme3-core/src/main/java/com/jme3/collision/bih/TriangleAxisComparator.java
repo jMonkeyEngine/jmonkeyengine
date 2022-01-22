f6918160e44cce368879c570d93879e51b719200
Stephen Gold <sgold@sonic.net>
2022-01-21 21:39:30 -0800

    TriangleAxisComparator:  one statement per line

--- jme3-core/src/main/java/com/jme3/collision/bih/TriangleAxisComparator.java
+++ jme3-core/src/main/java/com/jme3/collision/bih/TriangleAxisComparator.java
@@ -48,10 +48,21 @@ public class TriangleAxisComparator implements Comparator<BIHTriangle> {
         Vector3f c1 = o1.getCenter();
         Vector3f c2 = o2.getCenter();
         switch (axis) {
-            case 0: v1 = c1.x; v2 = c2.x; break;
-            case 1: v1 = c1.y; v2 = c2.y; break;
-            case 2: v1 = c1.z; v2 = c2.z; break;
-            default: assert false; return 0;
+            case 0:
+                v1 = c1.x;
+                v2 = c2.x;
+                break;
+            case 1:
+                v1 = c1.y;
+                v2 = c2.y;
+                break;
+            case 2:
+                v1 = c1.z;
+                v2 = c2.z;
+                break;
+            default:
+                assert false;
+                return 0;
         }
         if (v1 > v2)
             return 1;
