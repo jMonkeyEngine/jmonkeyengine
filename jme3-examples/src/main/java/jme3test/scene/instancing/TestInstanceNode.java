7aa805b969ff999d75ea3fa8291baa3eb184bbcb
Ali-RS <ali_codmw@yahoo.com>
2022-02-16 23:09:00 +0330

    Update copyright date.

--- jme3-examples/src/main/java/jme3test/scene/instancing/TestInstanceNode.java
+++ jme3-examples/src/main/java/jme3test/scene/instancing/TestInstanceNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2022 jMonkeyEngine
+ * Copyright (c) 2009-2021 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
