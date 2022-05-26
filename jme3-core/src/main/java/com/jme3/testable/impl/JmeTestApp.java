c2911eb3607015f7a2b9ad7aef297633f2a0d2c6
Scrappers <scrappers.tm@gmail.com>
2022-05-26 13:27:33 +0200

    JmeTestApp: Added java-docs and copyright

--- jme3-core/src/main/java/com/jme3/testable/impl/JmeTestApp.java
+++ jme3-core/src/main/java/com/jme3/testable/impl/JmeTestApp.java
@@ -1,10 +1,52 @@
+/*
+ * Copyright (c) 2009-2022 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.jme3.testable.impl;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.testable.Testable;
 
+/**
+ * A base implementation of the testable api for jMonkeyEngine Apps.
+ * 
+ * @author pavl_g
+ */
 public abstract class JmeTestApp extends SimpleApplication implements Testable {
 
+    /**
+     * Keeps track of the current test state. 
+     * True: if the current test is still running. 
+     * False: if the current test stops. 
+     * Default value: false, i.e: not active.
+     */
     protected volatile boolean active = false;
 
     @Override
