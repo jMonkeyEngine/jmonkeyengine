2572b7b28c26035f9e2234f3c5ccdbf34323ec23
Ali-RS <ali_codmw@yahoo.com>
2022-11-14 18:11:35 +0330

    Removed the bound-scale hack for shadow disappearing issue from DefaultInstanceCullingFunction. The “right” solution is to have it pay attention to the frustums of whatever shadow-casting lights are around… but anyway developers now can implement their own frustum culling however they like or even "unset" it to disable instance culling which should also resolve the shadow issue.

--- jme3-core/src/main/java/com/jme3/scene/instancing/InstancedGeometry.java
+++ jme3-core/src/main/java/com/jme3/scene/instancing/InstancedGeometry.java
@@ -477,76 +477,19 @@ public class InstancedGeometry extends Geometry {
 
     /**
      * By default, it checks if geometry is in camera frustum and culls it
-     * if it is outside camera view. A bound scale can be specified to scale
-     * geometry bound before checking against camera frustum. This can be used
-     * to prevent shadow disappearing when geometry gets slightly outside of
-     * camera frustum while it's shadow is still visible.
+     * if it is outside camera view.
      */
     public static class DefaultInstanceCullingFunction implements BiFunction<Camera, Geometry, Boolean> {
-
-        private Vector3f boundScale;
-        private final BoundingBox tempBound = new BoundingBox();
 
         @Override
         public Boolean apply(Camera cam, Geometry geom) {
             BoundingVolume bv = geom.getWorldBound();
-            if (boundScale != null) {
-                if (bv instanceof BoundingBox) {
-                    bv = getScaledBound(boundScale, (BoundingBox) bv, tempBound);
-                } else if (bv instanceof BoundingSphere) {
-                    bv = getScaledBound(boundScale, (BoundingSphere) bv, tempBound);
-                }
-            }
-
             int save = cam.getPlaneState();
             cam.setPlaneState(0);
             FrustumIntersect intersect = cam.contains(bv);
             cam.setPlaneState(save);
 
             return intersect == FrustumIntersect.Outside;
-        }
-
-        /**
-         * Sets the scale to be applied on instance bound before checking
-         * against camera frustum. Used to prevent shadow disappearing when
-         * geometry gets slightly outside of camera frustum while it's shadow
-         * is still visible.
-         *
-         * (default is null)
-         */
-        public void setBoundScale(Vector3f boundScale) {
-            this.boundScale = boundScale;
-        }
-
-        /**
-         * @return the bound scale (default is null)
-         */
-        public Vector3f getBoundScale() {
-            return boundScale;
-        }
-
-        protected BoundingBox getScaledBound(Vector3f scale, BoundingBox bound, BoundingBox store) {
-            if (store == null) {
-                store = new BoundingBox();
-            }
-
-            store.setCenter(bound.getCenter());
-            store.setXExtent(bound.getXExtent() * scale.x);
-            store.setYExtent(bound.getYExtent() * scale.y);
-            store.setZExtent(bound.getZExtent() * scale.z);
-            return store;
-        }
-
-        protected BoundingBox getScaledBound(Vector3f scale, BoundingSphere bound, BoundingBox store) {
-            if (store == null) {
-                store = new BoundingBox();
-            }
-
-            store.setCenter(bound.getCenter());
-            store.setXExtent(bound.getRadius() * scale.x);
-            store.setYExtent(bound.getRadius() * scale.y);
-            store.setZExtent(bound.getRadius() * scale.z);
-            return store;
         }
     }
 }
