73487fba1996188a7c093181951e835c27350c0d
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2022-12-15 22:05:29 +0200

    com.jme3.uitl.AreaUtils: removed code duplicates - added `final` class specifier

--- jme3-core/src/main/java/com/jme3/util/AreaUtils.java
+++ jme3-core/src/main/java/com/jme3/util/AreaUtils.java
@@ -35,6 +35,7 @@ import com.jme3.bounding.BoundingBox;
 import com.jme3.bounding.BoundingSphere;
 import com.jme3.bounding.BoundingVolume;
 import com.jme3.math.FastMath;
+import com.jme3.scene.control.AreaUtils;
 
 /**
  * <code>AreaUtils</code> is used to calculate the area of various objects, such as bounding volumes.  These
@@ -43,7 +44,7 @@ import com.jme3.math.FastMath;
  * @author Joshua Slack
  * @version $Id: AreaUtils.java 4131 2009-03-19 20:15:28Z blaine.dev $
  */
-public class AreaUtils {
+public final class AreaUtils {
     /**
      * A private constructor to inhibit instantiation of this class.
      */
@@ -60,29 +61,6 @@ public class AreaUtils {
      * @return The area in pixels on the screen of the bounding volume.
      */
     public static float calcScreenArea(BoundingVolume bound, float distance, float screenWidth) {
-        if (bound.getType() == BoundingVolume.Type.Sphere) {
-            return calcScreenArea((BoundingSphere) bound, distance, screenWidth);
-        } else if (bound.getType() == BoundingVolume.Type.AABB) {
-            return calcScreenArea((BoundingBox) bound, distance, screenWidth);
-        }
-        return 0.0f;
-    }
-
-    private static float calcScreenArea(BoundingSphere bound, float distance, float screenWidth) {
-        // Where is the center point and a radius point that lies in a plan parallel to the view plane?
-//    // Calc radius based on these two points and plug into circle area formula.
-//    Vector2f centerSP = null;
-//    Vector2f outerSP = null;
-//    float radiusSq = centerSP.subtract(outerSP).lengthSquared();
-        float radius = (bound.getRadius() * screenWidth) / (distance * 2);
-        return radius * radius * FastMath.PI;
-    }
-
-    private static float calcScreenArea(BoundingBox bound, float distance, float screenWidth) {
-        // Calc as if we are a BoundingSphere for now...
-        float radiusSquare = bound.getXExtent() * bound.getXExtent()
-                + bound.getYExtent() * bound.getYExtent()
-                + bound.getZExtent() * bound.getZExtent();
-        return ((radiusSquare * screenWidth * screenWidth) / (distance * distance * 4)) * FastMath.PI;
+        return AreaUtils.calcScreenArea(bound, distance, screenWidth);
     }
 }
