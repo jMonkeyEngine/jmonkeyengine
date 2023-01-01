5b9fc87234688bd74061a5fb134fdc2a6f526850
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-01-01 07:12:01 +0200

    AreaUtils: Migrated package to `com.jme3.util` (#1826)
    * AreaUtils: Migrated package to `com.jme3.util`
    
    * com.jme3.uitl.AreaUtils: removed code duplicates - added `final` class specifier
    
    * com.jme3.util.AreaUtils: fixed duplication build error
    
    * utils/AreaUtils.java: full migration to the utility package
    
    * scene/control/AreaUtils.java: removed utility methods - delegated functionality to `jme3.utils.AreaUtils`
    
    * scene/AreaUtils: fixed `jme3.util` package linking typo

--- jme3-core/src/main/java/com/jme3/util/AreaUtils.java
+++ jme3-core/src/main/java/com/jme3/util/AreaUtils.java
@@ -29,7 +29,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.jme3.scene.control;
+package com.jme3.util;
 
 import com.jme3.bounding.BoundingBox;
 import com.jme3.bounding.BoundingSphere;
@@ -39,18 +39,17 @@ import com.jme3.math.FastMath;
 /**
  * <code>AreaUtils</code> is used to calculate the area of various objects, such as bounding volumes.  These
  * functions are very loose approximations.
+ *
  * @author Joshua Slack
  * @version $Id: AreaUtils.java 4131 2009-03-19 20:15:28Z blaine.dev $
- * @deprecated use {@link com.jme3.util.AreaUtils} instead, due to wrong package
  */
-@Deprecated
-public class AreaUtils {
+public final class AreaUtils {
     /**
      * A private constructor to inhibit instantiation of this class.
      */
     private AreaUtils() {
     }
-
+    
     /**
     * Estimate the screen area of a bounding volume. If the volume isn't a
     * BoundingSphere, BoundingBox, or OrientedBoundingBox, 0 is returned.
@@ -61,6 +60,29 @@ public class AreaUtils {
     * @return The area in pixels on the screen of the bounding volume.
     */
     public static float calcScreenArea(BoundingVolume bound, float distance, float screenWidth) {
-        return com.jme3.util.AreaUtils.calcScreenArea(bound, distance, screenWidth);
+        if (bound.getType() == BoundingVolume.Type.Sphere) {
+            return calcScreenArea((BoundingSphere) bound, distance, screenWidth);
+        } else if (bound.getType() == BoundingVolume.Type.AABB) {
+            return calcScreenArea((BoundingBox) bound, distance, screenWidth);
+        }
+        return 0.0f;
+    }
+
+    private static float calcScreenArea(BoundingSphere bound, float distance, float screenWidth) {
+        // Where is the center point and a radius point that lies in a plan parallel to the view plane?
+        //    // Calc radius based on these two points and plug into circle area formula.
+        //    Vector2f centerSP = null;
+        //    Vector2f outerSP = null;
+        //    float radiusSq = centerSP.subtract(outerSP).lengthSquared();
+        float radius = (bound.getRadius() * screenWidth) / (distance * 2);
+        return radius * radius * FastMath.PI;
+    }
+
+    private static float calcScreenArea(BoundingBox bound, float distance, float screenWidth) {
+        // Calc as if we are a BoundingSphere for now...
+        float radiusSquare = bound.getXExtent() * bound.getXExtent()
+                         + bound.getYExtent() * bound.getYExtent()
+                         + bound.getZExtent() * bound.getZExtent();
+        return ((radiusSquare * screenWidth * screenWidth) / (distance * distance * 4)) * FastMath.PI;
     }
 }
