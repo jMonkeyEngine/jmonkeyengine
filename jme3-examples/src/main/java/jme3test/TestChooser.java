488d2008410b49fda27689466941bacc8ff664e6
Ali-RS <ali_codmw@yahoo.com>
2023-01-05 17:36:13 +0330

    Update copyright date.

--- jme3-examples/src/main/java/jme3test/TestChooser.java
+++ jme3-examples/src/main/java/jme3test/TestChooser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2021 jMonkeyEngine
+ * Copyright (c) 2009-2023 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
