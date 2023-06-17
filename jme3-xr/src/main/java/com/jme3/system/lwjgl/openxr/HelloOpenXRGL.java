81ae7722f667659ec21b30ce36fd69b5f35a057d
Starcommander <starcommander@gmx.at>
2023-06-17 16:35:45 +0200

    Change rendering from Clockwise to Counter-Clockwise as it is the default in jme3. Fixes the issue, that object are only visible from inner side.

--- jme3-xr/src/main/java/com/jme3/system/lwjgl/openxr/HelloOpenXRGL.java
+++ jme3-xr/src/main/java/com/jme3/system/lwjgl/openxr/HelloOpenXRGL.java
@@ -830,7 +830,7 @@ public class HelloOpenXRGL {
         );
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-        glFrontFace(GL_CW);
+        glFrontFace(GL_CCW);
         glCullFace(GL_BACK);
         glEnable(GL_DEPTH_TEST);
 
