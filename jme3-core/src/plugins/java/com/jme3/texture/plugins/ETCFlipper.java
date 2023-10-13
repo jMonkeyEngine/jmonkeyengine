10241c95ae5f747532a4e8af951e7666d4ca65a7
Riccardo Balbo <riccardo0blb@gmail.com>
2023-10-14 00:05:37 +0200

    Add compressonator ETC formats to DDS loader

---
+++ jme3-core/src/plugins/java/com/jme3/texture/plugins/ETCFlipper.java
@@ -0,0 +1,15 @@
+package com.jme3.texture.plugins;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
+
+import com.jme3.texture.Image.Format;
+
+public class ETCFlipper {
+    private static final Logger logger = Logger.getLogger(ETCFlipper.class.getName());
+    
+    public static ByteBuffer flipETC(ByteBuffer img, int w, int h, Format format) {
+        logger.warning("ETC texture flipping is not supported yet");
+        return img;
+    }
+}
