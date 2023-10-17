81c1362c1b590820e937558a17aacf10a18bcfee
chenliming <liming.chen@yingxiong.com>
2023-10-17 21:00:45 +0800

    jme3-examples: Added two renderPath sample codes. Added terrain unlit shading model test code; Added terrain lighting shading model test code;

--- jme3-examples/src/main/java/jme3test/renderpath/RenderPathHelper.java
+++ jme3-examples/src/main/java/jme3test/renderpath/RenderPathHelper.java
@@ -6,18 +6,34 @@ import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 
 public class RenderPathHelper implements ActionListener {
     private RenderManager.RenderPath currentRenderPath;
     private BitmapText hitText;
     private RenderManager renderManager;
+    private Vector3f hitLocation = new Vector3f(0, 0, 0);
+    private String keyHit = "SPACE";
+    private int keyInput = KeyInput.KEY_SPACE;
     public RenderPathHelper(SimpleApplication simpleApplication){
         renderManager = simpleApplication.getRenderManager();
         currentRenderPath = renderManager.getRenderPath();
         makeHudText(simpleApplication);
+        hitLocation.set(0, simpleApplication.getCamera().getHeight(), 0);
         simpleApplication.getInputManager().addListener(this, "toggleRenderPath");
-        simpleApplication.getInputManager().addMapping("toggleRenderPath", new KeyTrigger(KeyInput.KEY_SPACE));
+        simpleApplication.getInputManager().addMapping("toggleRenderPath", new KeyTrigger(keyInput));
+    }
+
+    public RenderPathHelper(SimpleApplication simpleApplication, Vector3f hitLocation, int keyInput, String keyHit){
+        renderManager = simpleApplication.getRenderManager();
+        currentRenderPath = renderManager.getRenderPath();
+        this.hitLocation.set(hitLocation);
+        this.keyInput = keyInput;
+        this.keyHit = keyHit;
+        makeHudText(simpleApplication);
+        simpleApplication.getInputManager().addListener(this, "toggleRenderPath");
+        simpleApplication.getInputManager().addMapping("toggleRenderPath", new KeyTrigger(keyInput));
     }
 
     private void makeHudText(SimpleApplication simpleApplication) {
@@ -25,14 +41,15 @@ public class RenderPathHelper implements ActionListener {
         hitText = new BitmapText(guiFont, false);
         hitText.setSize(guiFont.getCharSet().getRenderedSize());
         hitText.setText("RendererPath : "+ currentRenderPath.getInfo());
-        hitText.setLocalTranslation(0, simpleApplication.getCamera().getHeight(), 0);
+        hitText.setLocalTranslation(hitLocation);
         simpleApplication.getGuiNode().attachChild(hitText);
 
         // hit text
         BitmapText title = new BitmapText(guiFont, false);
         title.setSize(guiFont.getCharSet().getRenderedSize());
-        title.setText("Please press the SPACE to toggle the render path");
-        title.setLocalTranslation(0, simpleApplication.getCamera().getHeight() - 20, 0);
+        title.setText("Please press the " + keyHit + " to toggle the render path");
+        title.setLocalTranslation(hitLocation);
+        title.getLocalTranslation().y -= 20;
         simpleApplication.getGuiNode().attachChild(title);
     }
     @Override
