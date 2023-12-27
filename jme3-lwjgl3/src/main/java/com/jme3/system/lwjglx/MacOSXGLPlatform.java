67e029b250ba61b6eb2995f4d58356660db5c201
wil <wil@debian>
2023-12-27 15:04:22 -0600

    MacOS compatibility (no drastic changes)

--- jme3-lwjgl3/src/main/java/com/jme3/system/lwjglx/MacOSXGLPlatform.java
+++ jme3-lwjgl3/src/main/java/com/jme3/system/lwjglx/MacOSXGLPlatform.java
@@ -32,15 +32,25 @@
 package com.jme3.system.lwjglx;
 
 import org.lwjgl.opengl.awt.PlatformMacOSXGLCanvas;
+import static org.lwjgl.system.jawt.JAWTFunctions.*;
 
 /**
- *
+ * <code>MacOSXGLPlatform</code> class that implements the {@link com.jme3.system.lwjglx.LwjglxGLPlatform} 
+ * interface for the MacOS platform.
+ * 
  * @author wil
  */
 public class MacOSXGLPlatform extends PlatformMacOSXGLCanvas implements LwjglxGLPlatform {
 
+    /**
+     * (non-JavaDoc)
+     * @see com.jme3.system.lwjglx.LwjglxGLPlatform#destroy() 
+     */
     @Override
     public void destroy() {
-        
+        if (ds != null) {
+            JAWT_FreeDrawingSurface(ds, awt.FreeDrawingSurface());
+            awt.free();
+        }
     }
 }
