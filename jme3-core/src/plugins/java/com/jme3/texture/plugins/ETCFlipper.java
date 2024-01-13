9b4f7d771aecf1a1ab8cb5cf3894989385d125b9
SceneMax3D <scenemax3d@gmail.com>
2024-01-13 13:50:08 +0200

    ETCFlipper description

--- jme3-core/src/plugins/java/com/jme3/texture/plugins/ETCFlipper.java
+++ jme3-core/src/plugins/java/com/jme3/texture/plugins/ETCFlipper.java
@@ -5,6 +5,10 @@ import java.util.logging.Logger;
 
 import com.jme3.texture.Image.Format;
 
+/**
+ * A place-holder for future implementation of ETC texture flipping.
+ * 
+*/
 public class ETCFlipper {
     private static final Logger logger = Logger.getLogger(ETCFlipper.class.getName());
     
