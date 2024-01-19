70f9da9907ed02a990d938d3a2deddda1f8ffeb3
Riccardo Balbo <riccardo0blb@gmail.com>
2024-01-19 09:11:20 +0100

    Expand support for GL ES / WebGL compatible formats. (#2101)
    * Add more WebGL / GLES supported formats
    
    * Add compressonator ETC formats to DDS loader
    
    * ETCFlipper description
    
    ---------
    
    Co-authored-by: SceneMax3D <scenemax3d@gmail.com>

--- jme3-core/src/main/java/com/jme3/renderer/opengl/GLExt.java
+++ jme3-core/src/main/java/com/jme3/renderer/opengl/GLExt.java
@@ -45,6 +45,11 @@ public interface GLExt {
 
     public static final int GL_ALREADY_SIGNALED = 0x911A;
     public static final int GL_COMPRESSED_RGB8_ETC2 = 0x9274;
+    public static final int GL_COMPRESSED_SRGB8_ETC2 = 0x9275;
+    public static final int GL_COMPRESSED_RGBA8_ETC2_EAC = 0x9278;
+    public static final int GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC = 0x9279;
+    public static final int GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2 = 0x9276;
+    public static final int GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 = 0x9277;
     public static final int GL_COMPRESSED_RGBA_S3TC_DXT1_EXT = 0x83F1;
     public static final int GL_COMPRESSED_RGBA_S3TC_DXT3_EXT = 0x83F2;
     public static final int GL_COMPRESSED_RGBA_S3TC_DXT5_EXT = 0x83F3;
