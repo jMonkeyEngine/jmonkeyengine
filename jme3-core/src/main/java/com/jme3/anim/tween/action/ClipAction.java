23f49223d4844b10b42b2ef8fc94e47cdc765b81
Wyatt Gillette <jefferydeaver2010@gmail.com>
2024-02-09 12:12:00 +0100

    Update javadoc

--- jme3-core/src/main/java/com/jme3/anim/tween/action/ClipAction.java
+++ jme3-core/src/main/java/com/jme3/anim/tween/action/ClipAction.java
@@ -97,7 +97,7 @@ public class ClipAction extends BlendableAction {
     }
 
     /**
-     * Gets the animation clip associated with this object
+     * Gets the animation clip associated with this object.
      * 
      * @return The animation clip
      */
