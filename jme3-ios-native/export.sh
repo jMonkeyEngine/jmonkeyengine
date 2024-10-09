f35a2b46a9209ac98a17bde9dc00a24f44235e1f
joliver82 <joliver82@local>
2024-10-09 13:38:38 +0200

    Cleaned ios native project

--- jme3-ios-native/export.sh
+++ jme3-ios-native/export.sh
@@ -1,11 +1,11 @@
-rm -rf intermediate-builds release template/META-INF/robovm/ios/libs/jme3-ios-native-lib.xcframework
+rm -rf intermediate-builds release template/META-INF/robovm/ios/libs/jme3-ios-native.xcframework
 mkdir intermediate-builds release
-xcodebuild archive -project jme3-ios-native-lib.xcodeproj -scheme jme3-ios-native-lib -destination generic/platform=iOS -archivePath intermediate-builds/jme3-ios-native-lib_iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -project jme3-ios-native-lib.xcodeproj -scheme jme3-ios-native-lib -destination generic/platform="iOS Simulator" -archivePath intermediate-builds/jme3-ios-native-lib_iOS-Simulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -project jme3-ios-native.xcodeproj -scheme jme3-ios-native -destination generic/platform=iOS -archivePath intermediate-builds/jme3-ios-native_iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -project jme3-ios-native.xcodeproj -scheme jme3-ios-native -destination generic/platform="iOS Simulator" -archivePath intermediate-builds/jme3-ios-native_iOS-Simulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild -create-xcframework -framework intermediate-builds/jme3-ios-native-lib_iOS.xcarchive/Products/Library/Frameworks/jme3_ios_native_lib.framework -framework intermediate-builds/jme3-ios-native-lib_iOS-Simulator.xcarchive/Products/Library/Frameworks/jme3_ios_native_lib.framework -output template/META-INF/robovm/ios/libs/jme3-ios-native-lib.xcframework
+xcodebuild -create-xcframework -framework intermediate-builds/jme3-ios-native_iOS.xcarchive/Products/Library/Frameworks/jme3_ios_native.framework -framework intermediate-builds/jme3-ios-native_iOS-Simulator.xcarchive/Products/Library/Frameworks/jme3_ios_native.framework -output template/META-INF/robovm/ios/libs/jme3-ios-native.xcframework
 
 cd template
-zip -r ../release/jme3-ios-native-lib.jar META-INF
+zip -r ../release/jme3-ios-native.jar META-INF
 cd ..
 
--- jme3-ios-native/src/jme3_ios_native.h
+++ jme3-ios-native/src/jme3_ios_native.h
@@ -1,6 +1,6 @@
 //
-//  jme3_ios_native_lib.h
-//  jme3-ios-native-lib
+//  jme3_ios_native.h
+//  jme3-ios-native
 //
 //  Created by v1r7ua1 on 19/09/2024.
 //
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for jme3_ios_native_lib.
-FOUNDATION_EXPORT double jme3_ios_native_libVersionNumber;
+FOUNDATION_EXPORT double jme3_ios_nativeVersionNumber;
 
 //! Project version string for jme3_ios_native_lib.
-FOUNDATION_EXPORT const unsigned char jme3_ios_native_libVersionString[];
+FOUNDATION_EXPORT const unsigned char jme3_ios_nativeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <jme3_ios_native_lib/PublicHeader.h>
 
