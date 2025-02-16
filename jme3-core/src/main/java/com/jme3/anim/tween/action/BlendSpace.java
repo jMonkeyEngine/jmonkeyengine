b03dec8be3959153ce678da8fea7ae2756f01835
Pavly Gerges (pavl_g) <60224159+pavly-gerges@users.noreply.github.com>
2025-02-16 19:10:12 +0200

    BlendSpace: re-enforcements at the class-entry docs

--- jme3-core/src/main/java/com/jme3/anim/tween/action/BlendSpace.java
+++ jme3-core/src/main/java/com/jme3/anim/tween/action/BlendSpace.java
@@ -33,24 +33,28 @@ package com.jme3.anim.tween.action;
 
 /**
  * A provider interface which provides a value {@link BlendSpace#getWeight()} to control the blending between 2 successive actions in a {@link BlendAction}.
- * The blending weight is a read-only value, and it's manipulatable using the arbitrary value {@link BlendSpace#setValue(float)} during the application runtime.
+ * The blending weight is a read-only value, and it can be manipulated using the arbitrary value {@link BlendSpace#setValue(float)} during the application runtime.
  * 
  * <p>
  * Notes:
+ * <ul>
  * <li> Blending is the action of mixing between 2 successive animation {@link BlendableAction}s by interpolating their transforms and 
  * then applying the result on the assigned {@link HasLocalTransform} object, the {@link BlendSpace} provides this blending action with a blend weight value. </li>
  * <li> The blend weight is the value for the interpolation for the target transforms. </li>
  * <li> The blend weight value must be in this interval [0, 1]. </li>
+ * </ul>
  * </p>
  * 
  * <p>
  * Different blending weight case scenarios managed by {@link BlendAction} internally:
+ * <ul>
  * <li> In case of (0 < Blending weight < 1), the blending is executed each update among 2 actions, the first action will use 
  * a blend value of 1 and the second action will use the blend space weight as a value for the interpolation. </li>
  * <li> In case of (Blending weight = 0), the blending hasn't started yet, only the first action will be interpolated at (weight = 1). </li>
  * <li> In case of (Blending weight = 1), the blending is finished and only the second action will continue to run at (weight = 1). </li>
- * <li> Negative values and values greater than 1 aren't allowed (extrapolations aren't allowed). </li>
+ * <li> Negative values and values greater than 1 aren't allowed (i.e., extrapolations aren't allowed). </li>
  * <li> Find more at {@link BlendAction#doInterpolate(double)} and {@link BlendAction#collectTransform(HasLocalTransform, Transform, float, BlendableAction)}. </li>
+ * </ul>
  * </p>
  * 
  * Created by Nehon.
@@ -67,7 +71,7 @@ public interface BlendSpace {
 
     /**
      * Provides the blend weight value to the assigned {@link BlendAction} instance,
-     * this value will be used for interpolating a collection of actions' transformations (keyframes).
+     * this value will be used for interpolating a collection of actions' transformations (i.e., keyframes).
      * 
      * @return the blending weight value in the range from 0 to 1, 
      *         negative values and values above 1 aren't allowed.
