1dcfeb85afa14053f4806b6c2ce9b31eed8a6843
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2025-05-03 11:28:00 +0100

    #2416 Automate PBR Terrain tests

--- jme3-screenshot-tests/src/test/java/org/jmonkeyengine/screenshottests/light/pbr/TestPBRSimple.java
+++ jme3-screenshot-tests/src/test/java/org/jmonkeyengine/screenshottests/light/pbr/TestPBRSimple.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2024 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.jmonkeyengine.screenshottests.light.pbr;
 
 import com.jme3.app.Application;
@@ -6,7 +37,6 @@ import com.jme3.app.state.BaseAppState;
 import com.jme3.asset.AssetManager;
 import com.jme3.environment.EnvironmentProbeControl;
 import com.jme3.material.Material;
-import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Geometry;
@@ -51,7 +81,6 @@ public class TestPBRSimple extends ScreenshotTestBase {
         String imageName = testInfo.getTestClass().get().getName() + "." + testInfo.getTestMethod().get().getName() + "_" + testName;
 
         screenshotTest(new BaseAppState() {
-            private EnvironmentProbeControl envProbe;
             private int frame = 0;
             
             @Override
@@ -76,7 +105,7 @@ public class TestPBRSimple extends ScreenshotTestBase {
                 simpleApp.getRootNode().attachChild(sky);
 
                 // Create baker control
-                envProbe = new EnvironmentProbeControl(assetManager, 256);
+                EnvironmentProbeControl envProbe = new EnvironmentProbeControl(assetManager, 256);
                 simpleApp.getRootNode().addControl(envProbe);
                 
                 // Tag the sky, only the tagged spatials will be rendered in the env map
@@ -84,19 +113,13 @@ public class TestPBRSimple extends ScreenshotTestBase {
             }
 
             @Override
-            protected void cleanup(Application app) {
-                // Nothing to clean up
-            }
+            protected void cleanup(Application app) {}
 
             @Override
-            protected void onEnable() {
-                // Nothing to do
-            }
+            protected void onEnable() {}
 
             @Override
-            protected void onDisable() {
-                // Nothing to do
-            }
+            protected void onDisable() {}
 
             @Override
             public void update(float tpf) {
