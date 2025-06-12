88d35e5ea9305cb41df786d611ed9e37f245aac7
Wyatt Gillette <jefferydeaver2010@gmail.com>
2025-06-12 21:08:38 +0200

    Update Materials: add Billboard shader

--- jme3-core/src/main/java/com/jme3/material/Materials.java
+++ jme3-core/src/main/java/com/jme3/material/Materials.java
@@ -43,6 +43,7 @@ public class Materials {
     public static final String LIGHTING     = "Common/MatDefs/Light/Lighting.j3md";
     public static final String PBR          = "Common/MatDefs/Light/PBRLighting.j3md";
     public static final String PARTICLE     = "Common/MatDefs/Misc/Particle.j3md";
+    public static final String BILLBOARD    = "Common/MatDefs/Misc/Billboard.j3md";
 
     /**
      * A private constructor to inhibit instantiation of this class.
