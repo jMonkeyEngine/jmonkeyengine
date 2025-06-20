146835b0f9f297fec3b43bd4395ea31b7109af98
Wyatt Gillette <jefferydeaver2010@gmail.com>
2025-06-20 11:37:50 +0200

    Update ColorRGBATest.java

--- jme3-core/src/test/java/com/jme3/math/ColorRGBATest.java
+++ jme3-core/src/test/java/com/jme3/math/ColorRGBATest.java
@@ -22,15 +22,9 @@ public class ColorRGBATest {
 
         ColorRGBA copy = new ColorRGBA();
 
-        validateColor(color, copy.fromIntRGBA(rgba));
-        validateColor(color, copy.fromIntABGR(abgr));
-        validateColor(color, copy.fromIntARGB(argb));
+        Assert.assertEquals(color, copy.fromIntRGBA(rgba));
+        Assert.assertEquals(color, copy.fromIntABGR(abgr));
+        Assert.assertEquals(color, copy.fromIntARGB(argb));
     }
 
-    private void validateColor(ColorRGBA original, ColorRGBA copy) {
-        Assert.assertEquals(original.r, copy.r, 0.001f);
-        Assert.assertEquals(original.g, copy.g, 0.001f);
-        Assert.assertEquals(original.b, copy.b, 0.001f);
-        Assert.assertEquals(original.a, copy.a, 0.001f);
-    }
 }
