5adc84a09b7ba2d3e657834b32f8fe0195d1c573
Ryan McDonough <peanut64646@gmail.com>
2026-01-05 01:57:39 -0500

    Add NoiseVectorEffect.java

---
+++ jme3-effects/src/main/java/com/jme3/vectoreffect/NoiseVectorEffect.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2009-2026 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.vectoreffect;
+
+import com.jme3.math.Vector4f;
+import com.jme3.math.FastNoiseLite;
+import com.jme3.math.FastNoiseLite.NoiseType;
+
+/**
+ *
+ * @author yaRnMcDonuts
+ */
+public class NoiseVectorEffect extends VectorEffect {
+    
+    private FastNoiseLite noiseGenerator;        
+
+    private VectorGroup noiseMagnitudes;    
+    private VectorGroup originalVectorValues;
+    
+    public float speed = 1;
+    private float timeAccrued = 0;
+    
+    public NoiseVectorEffect(Object vectorObject, Object noiseMagnitude) {
+        this(vectorObject, noiseMagnitude, NoiseType.OpenSimplex2, 0.5f);       
+    }
+    
+    public NoiseVectorEffect(Object vectorObject, Object noiseMagnitude, NoiseType noiseType, float frequency) {
+        super(vectorObject);
+        
+        if(noiseMagnitude instanceof VectorGroup){
+            noiseMagnitudes = (VectorGroup) noiseMagnitude;
+        }else{
+            noiseMagnitudes = new VectorGroup(noiseMagnitude);
+        }        
+        
+        noiseGenerator = new FastNoiseLite();
+        noiseGenerator.SetFrequency(frequency);
+        noiseGenerator.SetNoiseType(noiseType);       
+      
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+        
+        if(originalVectorValues == null){
+            originalVectorValues = new VectorGroup();
+            for(int v = 0; v < vectorsToModify.getSize(); v++){
+                Vector4f startVector = vectorsToModify.getAsVector4(v).clone();
+                originalVectorValues.addVectorToGroup(startVector);
+            }
+
+        }
+        
+        timeAccrued += tpf;                
+        
+        //to-do: add a togglable isUniform boolean that can be set false to allow each x/y/z/w component to be altered by a different noise value.
+        
+        float noiseReturnVal = noiseGenerator.GetNoise(timeAccrued * speed, 12.671f + timeAccrued * speed * 0.92173f, 19.54f + timeAccrued * speed * 0.68913f);
+        
+        for(int v = 0; v < vectorsToModify.getSize(); v++){
+            int magnitudeIndex = Math.min(v, noiseMagnitudes.getSize() - 1); //allows multiple vectors to share the same magnitude if desired
+            Vector4f noiseVariation = noiseMagnitudes.getAsVector4(magnitudeIndex).mult(noiseReturnVal);
+        
+            noiseVariation.addLocal(originalVectorValues.getAsVector4(v));
+        
+            vectorsToModify.updateVectorObject(noiseVariation, v);
+        }             
+    }    
+    
+    public FastNoiseLite getNoiseGenerator() {
+        return noiseGenerator;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+    
+    @Override
+    public void reset() {
+        super.reset(); 
+        originalVectorValues = null;
+    }
+}
