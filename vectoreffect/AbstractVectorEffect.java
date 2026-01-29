d92151ca2cb488d3bab0cbe7a75fdfc23b6df6ac
Ryan McDonough <peanut64646@gmail.com>
2026-01-29 15:30:25 -0500

    Add files via upload

---
+++ vectoreffect/AbstractVectorEffect.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2009-2026 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jme3.vectoreffect;
+
+import com.jme3.app.state.AppStateManager;
+import java.util.ArrayList;
+
+/**
+ * @author yaRnMcDonuts
+ */
+
+public abstract class AbstractVectorEffect implements VectorEffect {
+    
+    protected VectorGroup vectorsToModify;    
+    private final ArrayList<Runnable> onFinishedCallbacks = new ArrayList<>();    
+    protected boolean isFinished = false;       
+   
+    public AbstractVectorEffect(){
+        
+    }
+    
+    public AbstractVectorEffect(VectorGroup vectorsToModify) {
+        this.vectorsToModify = vectorsToModify;
+    }        
+    
+    @Override
+    public void setIsFinished(boolean isFinished) {        
+        this.isFinished = isFinished;
+        if (isFinished) {
+            for(Runnable r : onFinishedCallbacks) {
+                r.run();
+            }
+            onFinishedCallbacks.clear();
+        }
+    }
+    
+    @Override
+    public VectorGroup getVectorsToModify(){
+        return vectorsToModify;
+    }
+    
+    @Override
+    public boolean isFinished() {        
+        return isFinished;  
+    }    
+    
+
+    @Override
+    public void reset() {
+        isFinished = false;
+    }
+    
+    
+    public void registerRunnableOnFinish(Runnable runnable) {
+        onFinishedCallbacks.add(runnable);
+    }  
+    
+    @Override
+    public void update(float tpf){
+       
+    }    
+        
+    // convenience registration method so users can avoid repeatedly writing this AppState fetching code
+    public void convenienceRegister(AppStateManager stateManager) {
+        if(stateManager != null){
+            VectorEffectManagerState vectorEffectManagerState = stateManager.getState(VectorEffectManagerState.class);
+            if(vectorEffectManagerState != null){
+                vectorEffectManagerState.registerVectorEffect(this);
+            }
+        }
+    }
+
+
+}
+
