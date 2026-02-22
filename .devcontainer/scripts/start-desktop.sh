b4be4ede929bf4fbd4c65911e646c9575f7a1b65
Riccardo Balbo <os@rblb.it>
2026-02-22 15:49:52 +0100

    improvements

--- .devcontainer/scripts/start-desktop.sh
+++ .devcontainer/scripts/start-desktop.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+if [ "$JME_DEV_ENVIRONMENT" != "yesReally" ];
+then
+    echo "Not running! This script is meant to be run in the jMonkeyEngine dev container environment, and may do unexpected things if run elsewhere."
+    exit 1
+fi
+ 
 export DISPLAY="${DISPLAY:-:99}"
 unset WAYLAND_DISPLAY WAYLAND_SOCKET XDG_RUNTIME_DIR
 export GDK_BACKEND=x11
@@ -18,7 +23,7 @@ fi
 
 # Start virtual X server
 if ! pgrep -f "Xvfb ${DISPLAY}" >/dev/null 2>&1; then
-  nohup Xvfb "${DISPLAY}" -screen 0 1280x720x24 +extension GLX +render -noreset >/tmp/xvfb.log 2>&1 &
+  nohup Xvfb "${DISPLAY}" -screen 0 1440x900x24 +extension GLX +render -noreset >/tmp/xvfb.log 2>&1 &
 fi
 
 # Start a dbus session (XFCE wants it)
@@ -27,14 +32,35 @@ if ! pgrep -u "$(id -u)" -f "dbus-daemon.*--session" >/dev/null 2>&1; then
   eval "$(dbus-launch --sh-syntax)"
 fi
 
+
+
+if [ "$JME_DEV_ENVIRONMENT" != "yesReally" ];
+then
+    echo "Not running! This script is meant to be run in the jMonkeyEngine dev container environment, and may do unexpected things if run elsewhere."
+    exit 1
+fi
+ 
+# Init home
+mkdir -p "$HOME/.config" "$HOME/.cache" "$HOME/.local/share"
+
+
+# Start window manager
+if ! pgrep -u "$(id -u)" -x xfwm4 >/dev/null 2>&1; then
+  nohup xfwm4 --compositor=off --daemon >/tmp/xfwm4.log 2>&1 &
+fi
+
+
 # Start XFCE session
 if ! pgrep -u "$(id -u)" -x xfce4-session >/dev/null 2>&1; then
   nohup xfce4-session >/tmp/xfce4-session.log 2>&1 &
 fi
 
+# Set wallpaper
+feh --no-fehbg --bg-fill "/root/wallpaper.jpg"
+
 # Start VNC server (no password; fine for Codespaces)
 if ! pgrep -f "x11vnc.*-rfbport 5900" >/dev/null 2>&1; then
-  nohup x11vnc -display "${DISPLAY}" -nopw -forever -shared -rfbport 5900 >/tmp/x11vnc.log 2>&1 &
+  nohup x11vnc -display "${DISPLAY}" -nopw -forever -shared -cursor arrow -rfbport 5900 >/tmp/x11vnc.log 2>&1 &
 fi
 
 # Start noVNC (websockify)
