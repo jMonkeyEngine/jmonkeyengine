55e4b8e4abcb09e62f2495bd19e5dc1bec30f77c
Riccardo Balbo <os@rblb.it>
2026-05-11 13:08:21 +0200

    move native image metadata generator to a reusable plugin

---
+++ jme3-nativemetadata-plugin/src/main/groovy/org/jmonkeyengine/gradle/nativemetadata/JmeNativeMetadataPlugin.groovy
@@ -0,0 +1,35 @@
+package org.jmonkeyengine.gradle.nativemetadata
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import java.security.MessageDigest
+
+/**
+ * Applies jMonkeyEngine's GraalVM native metadata conventions.
+ */
+class JmeNativeMetadataPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        URL metadataScript = getClass().getResource('/org/jmonkeyengine/gradle/nativemetadata/native-image-metadata.gradle')
+        if (metadataScript == null) {
+            throw new IllegalStateException('Unable to locate bundled native metadata Gradle script.')
+        }
+        project.extensions.extraProperties.set('jmeNativeMetadataGeneratorScriptHash', sha256(metadataScript))
+        project.apply(from: metadataScript)
+    }
+
+    private static String sha256(URL resource) {
+        MessageDigest digest = MessageDigest.getInstance('SHA-256')
+        resource.openStream().withCloseable { InputStream input ->
+            byte[] buffer = new byte[8192]
+            int bytesRead = input.read(buffer)
+            while (bytesRead != -1) {
+                digest.update(buffer, 0, bytesRead)
+                bytesRead = input.read(buffer)
+            }
+        }
+        return digest.digest().collect { String.format('%02x', it & 0xff) }.join()
+    }
+}
