5331aa38a5d8b1c61eb1d148b5405b1e68639986
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2026-05-24 12:11:16 +0100

    Remove redundant request to write to external storage

--- jme3-screenshot-tests/jme3-screenshot-tests-android/src/androidTest/java/org/jmonkeyengine/screenshottests/android/android/ScreenshotTestAndroidBase.java
+++ jme3-screenshot-tests/jme3-screenshot-tests-android/src/androidTest/java/org/jmonkeyengine/screenshottests/android/android/ScreenshotTestAndroidBase.java
@@ -9,10 +9,4 @@ public abstract class ScreenshotTestAndroidBase extends ScreenshotTestBase {
 
     @Rule
     public ExtentReportExtensionJunit4 extentReportExtension = new ExtentReportExtensionJunit4();
-
-    @Rule
-    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            android.Manifest.permission.READ_EXTERNAL_STORAGE);
-
 }
