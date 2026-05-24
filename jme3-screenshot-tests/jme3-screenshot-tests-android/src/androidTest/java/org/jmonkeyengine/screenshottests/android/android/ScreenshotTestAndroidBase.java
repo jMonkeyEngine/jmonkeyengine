0c0e209f98c1a004836b6a7155acdc882793ed08
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2026-05-24 17:44:21 +0100

    Try for a wider device

--- jme3-screenshot-tests/jme3-screenshot-tests-android/src/androidTest/java/org/jmonkeyengine/screenshottests/android/android/ScreenshotTestAndroidBase.java
+++ jme3-screenshot-tests/jme3-screenshot-tests-android/src/androidTest/java/org/jmonkeyengine/screenshottests/android/android/ScreenshotTestAndroidBase.java
@@ -1,8 +1,7 @@
 package org.jmonkeyengine.screenshottests.android.android;
 
-import androidx.test.rule.GrantPermissionRule;
-
 import org.jmonkeyengine.screenshottests.testframework.ScreenshotTestBase;
+
 import org.junit.Rule;
 
 public abstract class ScreenshotTestAndroidBase extends ScreenshotTestBase {
